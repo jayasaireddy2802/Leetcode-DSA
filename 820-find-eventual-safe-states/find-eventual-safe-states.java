833a748844eb6bf8e8f2ba2edbe60437b950fb2e
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-02-08 17:46:33 +0530

    Time: 25 ms (20.39%) | Memory: 55.6 MB (38.85%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,47 +1,102 @@
+// Using dfs
+
+// class Solution {
+
+//     public boolean dfs(int i, boolean[] visited, boolean[] pathVisited, int[][] graph)
+//     {
+//         visited[i] = true;
+//         pathVisited[i] = true;
+
+//         for(int neighbour : graph[i])
+//         {
+//             if(!visited[neighbour])
+//             {
+//                 if(dfs(neighbour, visited, pathVisited, graph))
+//                     return true;
+
+//             }
+//             else if (pathVisited[neighbour])
+//                 return true;
+//         }
+
+//         pathVisited[i] = false;
+//         return false;
+//     }
+
+//     public List<Integer> eventualSafeNodes(int[][] graph) {
+//         int v = graph.length;
+//         boolean[] visited = new boolean[v];
+//         boolean[] pathVisited = new boolean[v];
+//         List<Integer> ans = new ArrayList<>();
+
+//         for(int i = 0; i < v; i++)
+//         {
+//             if(!visited[i])
+//             {
+//                 dfs(i, visited, pathVisited, graph);
+//             }
+
+//         }
+
+//         for(int i = 0; i < v; i++)
+//         {
+//             if(!pathVisited[i])
+//                 ans.add(i);
+//         }
+
+//         return ans;
+//     }
+// }
+
+// using bfs (kahn's algo)
+
 class Solution {
-
-    public boolean dfs(int i, boolean[] visited, boolean[] pathVisited, int[][] graph)
-    {
-        visited[i] = true;
-        pathVisited[i] = true;
-
-        for(int neighbour : graph[i])
-        {
-            if(!visited[neighbour])
-            {
-                if(dfs(neighbour, visited, pathVisited, graph))
-                    return true;
-
-            }
-            else if (pathVisited[neighbour])
-                return true;
-        }
-
-        pathVisited[i] = false;
-        return false;
-    }
-
     public List<Integer> eventualSafeNodes(int[][] graph) {
+        List<List<Integer>> adj = new ArrayList<>();
         int v = graph.length;
-        boolean[] visited = new boolean[v];
-        boolean[] pathVisited = new boolean[v];
+        int[] indegre = new int[v];
         List<Integer> ans = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i = 0; i < v; i++)
+            adj.add(new ArrayList<>());
 
         for(int i = 0; i < v; i++)
         {
-            if(!visited[i])
+            for(int x : graph[i])
             {
-                dfs(i, visited, pathVisited, graph);
+                adj.get(x).add(i);
+                indegre[i]++;
             }
-
         }
 
         for(int i = 0; i < v; i++)
         {
-            if(!pathVisited[i])
+            if(indegre[i] == 0)
+            {
+                queue.add(i);
                 ans.add(i);
+            }
         }
 
+        while(!queue.isEmpty())
+        {
+            int node = queue.poll();
+
+            for(int neighbour : adj.get(node))
+            {
+                indegre[neighbour]--;
+                if(indegre[neighbour] == 0)
+                {
+                    queue.add(neighbour);
+                    ans.add(neighbour);
+                }
+
+            }
+        }
+
+        Collections.sort(ans);
         return ans;
     }
+
 }
