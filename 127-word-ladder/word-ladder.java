3a2187607ef3c2ee6a5051fb94ea9656835b8334
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-02-10 18:50:14 +0530

    Time: 98 ms (40.66%) | Memory: 47.3 MB (37.27%) - LeetSync

---
+++ 127-word-ladder/word-ladder.java
@@ -0,0 +1,55 @@
+class Pair
+{
+    int lvl;
+    String str;
+
+    public Pair(String str, int lvl)
+    {
+        this.lvl = lvl;
+        this.str = str;
+    }
+}
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set = new HashSet<>();
+        Queue<Pair> queue = new LinkedList<>();
+        int wordsLength = wordList.size();
+
+        for(String str : wordList)
+            set.add(str);
+
+        queue.add(new Pair(beginWord, 1));
+        set.remove(beginWord);
+
+        while(!queue.isEmpty())
+        {
+            Pair p = queue.poll();
+            int lvl = p.lvl;
+            String str = p.str;
+            int strLength = str.length();
+
+            if(str.equals(endWord))
+                return lvl;
+
+            for(int i = 0; i < strLength; i++)
+            {
+                for(char ch = 'a'; ch <= 'z'; ch++)
+                {
+                    char[] arr = str.toCharArray();
+                    arr[i] = ch;
+                    String newStr = new String(arr);
+
+                    if(set.contains(newStr))
+                    {
+                        set.remove(newStr);
+                        queue.add(new Pair(newStr, lvl + 1));
+                    }
+                }
+            }
+
+        }
+
+        return 0;
+    }
+}
