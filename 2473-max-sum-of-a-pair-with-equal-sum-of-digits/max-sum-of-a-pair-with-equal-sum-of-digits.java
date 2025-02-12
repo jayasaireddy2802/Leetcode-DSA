67e6ae0c4d545aed2b28fcffa21348f4855f509d
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-02-12 12:07:09 +0530

    Time: 42 ms (65.91%) | Memory: 59.6 MB (60.81%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -67,6 +67,56 @@
 // }
 
 
+// class Solution {
+
+//     public int digitsSum(int num)
+//     {
+//         int sum = 0;
+//         while(num > 0)
+//         {
+//             sum = sum + (num % 10);
+//             num = num / 10;
+//         }
+
+//         return sum;
+//     }
+
+//     public int maximumSum(int[] nums) {
+//         int n = nums.length;
+//         int ans = -1;
+//         Map<Integer, List<Integer>> map =new HashMap<>();
+//         for(int i = 0; i < n; i++)
+//         {
+//             int sum = digitsSum(nums[i]);
+//             List<Integer> list;
+//             if(!map.containsKey(sum))
+//             {
+//                 list = new ArrayList<>();
+//             }
+//             else
+//             {
+//                 list = map.get(sum);
+//             }
+//             list.add(nums[i]);
+//             map.put(sum, list);
+//         }
+
+//         for(int key : map.keySet())
+//         {
+//             List<Integer> list = map.get(key);
+//             int size = list.size();
+//             if(size < 2)
+//                 continue;
+//             Collections.sort(list);
+//             int num1 = list.get(size - 1);
+//             int num2 = list.get(size - 2);
+//             ans = Math.max(ans, num1 + num2);
+//         }
+
+//         return ans;
+//     }
+// }
+
 class Solution {
 
     public int digitsSum(int num)
@@ -84,35 +134,32 @@ class Solution {
     public int maximumSum(int[] nums) {
         int n = nums.length;
         int ans = -1;
-        int[] sum = new int[n];
-        Map<Integer, List<Integer>> map =new HashMap<>();
+        Map<Integer, int[]> map =new HashMap<>();
+        int sum = 0;
         for(int i = 0; i < n; i++)
         {
-            sum[i] = digitsSum(nums[i]);
-            List<Integer> list;
-            if(!map.containsKey(sum[i]))
+            sum = digitsSum(nums[i]);
+            int[] arr = map.getOrDefault(sum, new int[]{-1, -1});
+
+            if(nums[i] > arr[0])
             {
-                list = new ArrayList<>();
+                arr[1] = arr[0];
+                arr[0] = nums[i];
             }
-            else
+            else if(nums[i] > arr[1])
             {
-                list = map.get(sum[i]);
+                arr[1] = nums[i];
             }
-            list.add(nums[i]);
-            map.put(sum[i], list);
+
+            if(arr[1] != -1)
+            {
+                ans = Math.max(ans, arr[0] +  arr[1]);
+            }
+
+            map.put(sum, arr);
         }
 
-        for(int key : map.keySet())
-        {
-            List<Integer> list = map.get(key);
-            int size = list.size();
-            if(size < 2)
-                continue;
-            Collections.sort(list);
-            int num1 = list.get(size - 1);
-            int num2 = list.get(size - 2);
-            ans = Math.max(ans, num1 + num2);
-        }
+        
 
         return ans;
     }
