d0cc31bf95f6ad44cd22750ec4092a7b1a67f892
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-03-01 10:18:10 +0530

    Time: 1 ms (96.37%) | Memory: 43.8 MB (56.58%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
@@ -33,28 +33,64 @@
 // }
 
 
+// class Solution {
+//     public int[] applyOperations(int[] nums) {
+//         int len = nums.length;
+
+//         for(int i = 0; i < len-1; i++)
+//         {
+//             if(nums[i] == nums[i+1])
+//             {
+//                 nums[i] = nums[i]*2;
+//                 nums[i+1] = 0;
+//             }
+//         }
+
+//        int idx = 0;
+//        for(int i = 0; i < len; i++)
+//        {
+//             if(nums[i] != 0)
+//             {
+//                 nums[idx] = nums[i];
+//                 idx++;
+//             }
+//        }
+
+//         while(idx < len)
+//         {
+//             nums[idx] = 0;
+//             idx++;
+//         }
+
+//         return nums;
+//     }
+// }
+
 class Solution {
     public int[] applyOperations(int[] nums) {
         int len = nums.length;
 
-        for(int i = 0; i < len-1; i++)
+       int idx = 0;
+       int i = 0;
+        for( i = 0; i < len-1; i++)
         {
             if(nums[i] == nums[i+1])
             {
                 nums[i] = nums[i]*2;
                 nums[i+1] = 0;
             }
-        }
 
-       int idx = 0;
-       for(int i = 0; i < len; i++)
-       {
             if(nums[i] != 0)
             {
                 nums[idx] = nums[i];
                 idx++;
             }
-       }
+        }
+         if(nums[i] != 0)
+         {
+            nums[idx] = nums[i];
+            idx++;
+        }
 
         while(idx < len)
         {
