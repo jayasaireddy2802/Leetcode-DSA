ead053a2cfddbdd7ca9757039c430110305f3c62
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-03-02 17:51:39 +0530

    Time: 2 ms (46.64%) | Memory: 48.3 MB (20.60%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.java
@@ -0,0 +1,95 @@
+// class Solution {
+
+//     public void dfs(int i, boolean[] visited, int[][] isConnected)
+//     {
+//         visited[i] = true;
+//         int n = isConnected.length;
+//         for(int j = 0; j < n; j++)
+//         {
+//             if(isConnected[i][j] == 1 && !visited[j])
+//                 dfs(j, visited, isConnected);
+//         }
+
+//     }
+
+//     public int findCircleNum(int[][] isConnected) {
+//         int r = isConnected.length;
+//         int c = isConnected[0].length;
+//         boolean[] visited = new boolean[r];
+//         int ans = 0;
+//         for(int i = 0; i < r; i++)
+//         {
+//             if(!visited[i])
+//             {
+//                 dfs(i, visited, isConnected);
+//                 ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
+class DisjointSet
+{
+    int[] parent;
+    int[] rank;
+
+    public DisjointSet(int V)
+    {
+        rank = new int[V];
+        parent = new int[V];
+        
+        for(int i = 0; i < V; i++)
+        {
+            parent[i] = i;
+        }
+    }
+
+    public int findParent(int x)
+    {
+        if(parent[x] == x)
+            return x;
+        return parent[x] = findParent(parent[x]);
+    }
+
+    public void findURank(int u, int v)
+    {
+        int parent_u = findParent(u);
+        int parent_v = findParent(v);
+        if(parent_u == parent_v)
+            return ;
+        if(rank[parent_u] >= rank[parent_v])
+        {
+            parent[parent_v] = parent_u;
+        }
+        else
+        {
+            parent[parent_u] = parent_v;
+        }
+    }
+}
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int V = isConnected.length;
+        DisjointSet ds = new DisjointSet(V);
+
+        for(int i = 0;i < V; i++)
+        {
+            for(int j = 0; j < V; j++)
+            {
+                if(isConnected[i][j] == 1)
+                    ds.findURank(i, j);
+            }
+        }
+
+        int ct = 0;
+        for(int i = 0; i < V; i++)
+        {
+            if(ds.parent[i] == i)
+                ct++;
+        }
+
+        return ct;
+    }
+}
