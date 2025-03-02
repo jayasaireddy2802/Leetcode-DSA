953024a1675249314c51024f5921a1ec0b9fb703
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-03-02 10:53:28 +0530

    Time: 1 ms (86.98%) | Memory: 45.4 MB (61.98%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -79,74 +79,122 @@
 // }
 
 
+// class Solution {
+//     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+//         Map<Integer, Integer> map = new LinkedHashMap<>();
+//         int len1 = nums1.length;
+//         int len2 = nums2.length;
+//         int i = 0, j = 0;
+
+//         while(i < len1 && j < len2)
+//         {
+//             if(nums1[i][0] < nums2[j][0])
+//             {
+//                 if(map.containsKey(nums1[i][0]))
+//                 {
+//                     map.put(nums1[i][0], map.get(nums1[i][0]) + nums1[i][1]);
+//                 }
+//                 else
+//                     map.put(nums1[i][0], nums1[i][1]);
+//                 i++;
+//             }
+//             else
+//             {
+//                 if(map.containsKey(nums2[j][0]))
+//                 {
+//                     map.put(nums2[j][0], map.get(nums2[j][0]) + nums2[j][1]);
+//                 }
+//                 else
+//                     map.put(nums2[j][0], nums2[j][1]);
+//                 j++;
+//             }
+//         }
+
+//         while(i < len1)
+//         {
+//             if(map.containsKey(nums1[i][0]))
+//             {
+//                 map.put(nums1[i][0], map.get(nums1[i][0]) + nums1[i][1]);
+//             }
+//             else
+//                 map.put(nums1[i][0], nums1[i][1]);
+//             i++;
+//         }
+
+//         while(j < len2)
+//         {
+//             if(map.containsKey(nums2[j][0]))
+//             {
+//                 map.put(nums2[j][0], map.get(nums2[j][0]) + nums2[j][1]);
+//             }
+//             else
+//                 map.put(nums2[j][0], nums2[j][1]);
+//             j++;
+//         }
+        
+//         int size = map.size();
+//         int[][] res = new int[size][2];
+
+//         int k = 0;
+//         for(int x : map.keySet())
+//         {
+//             res[k][0] = x;
+//             res[k][1] = map.get(x);
+//             k++;
+//         }
+
+        
+//         return res;
+//     }
+// }
+
+
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        Map<Integer, Integer> map = new LinkedHashMap<>();
+        ArrayList<int[]> list = new ArrayList<>();
         int len1 = nums1.length;
         int len2 = nums2.length;
         int i = 0, j = 0;
 
         while(i < len1 && j < len2)
         {
-            if(nums1[i][0] < nums2[j][0])
+            if(nums1[i][0] == nums2[j][0])
             {
-                if(map.containsKey(nums1[i][0]))
-                {
-                    map.put(nums1[i][0], map.get(nums1[i][0]) + nums1[i][1]);
-                }
-                else
-                    map.put(nums1[i][0], nums1[i][1]);
+                list.add(new int[]{nums1[i][0], nums1[i][1] + nums2[j][1]});
+                i++;
+                j++;
+            }
+            else if(nums1[i][0] < nums2[j][0])
+            {
+                list.add(new int[]{nums1[i][0], nums1[i][1]});
                 i++;
             }
             else
             {
-                if(map.containsKey(nums2[j][0]))
-                {
-                    map.put(nums2[j][0], map.get(nums2[j][0]) + nums2[j][1]);
-                }
-                else
-                    map.put(nums2[j][0], nums2[j][1]);
+                list.add(new int[]{nums2[j][0], nums2[j][1]});
                 j++;
             }
         }
 
         while(i < len1)
         {
-            if(map.containsKey(nums1[i][0]))
-            {
-                map.put(nums1[i][0], map.get(nums1[i][0]) + nums1[i][1]);
-            }
-            else
-                map.put(nums1[i][0], nums1[i][1]);
+            list.add(new int[]{nums1[i][0], nums1[i][1]});
             i++;
         }
 
         while(j < len2)
         {
-            if(map.containsKey(nums2[j][0]))
-            {
-                map.put(nums2[j][0], map.get(nums2[j][0]) + nums2[j][1]);
-            }
-            else
-                map.put(nums2[j][0], nums2[j][1]);
+            list.add(new int[]{nums2[j][0], nums2[j][1]});
             j++;
         }
-        
-        int size = map.size();
-        int[][] res = new int[size][2];
 
-        int k = 0;
-        for(int x : map.keySet())
-        {
-            res[k][0] = x;
-            res[k][1] = map.get(x);
-            k++;
-        }
-
-        
-        return res;
+                
+        return list.toArray(new int[list.size()][]);
     }
 }
+
+
+
 
 
 
