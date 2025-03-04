ad8994dedafb528328c49cdbd28641dc684f3d4d
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-03-04 12:03:58 +0530

    Time: 475 ms (6.98%) | Memory: 41.1 MB (9.30%) - LeetSync

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public boolean solve(int idx, int val, int n, long[] prefix)
+    {
+        if(idx >= 30)
+            return false;
+        if(val > n)
+            return false;
+        if(val == n)
+            return true;
+
+        boolean nottake = solve(idx+1, val, n, prefix);
+        boolean take = solve(idx + 1, val + (int)prefix[idx], n, prefix);
+
+        return nottake || take;
+    }
+
+    public boolean checkPowersOfThree(int n) {
+        long[] prefix = new long[30];
+        prefix[0] = 1;
+
+        for(int i = 1; i < 30; i++)
+        {
+            prefix[i] = prefix[i-1]*3;
+        }
+        
+        return solve(0, 0, n, prefix);
+    }
+}
