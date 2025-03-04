a33a200d88c3423bfa808b245e92519fdd30a489
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-03-04 13:10:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (93.90%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,29 +1,42 @@
-class Solution {
+// class Solution {
 
-    public boolean solve(int idx, int val, int n, long[] prefix)
-    {
-        if(idx >= 30)
-            return false;
-        if(val > n)
-            return false;
-        if(val == n)
-            return true;
+//     public boolean solve(int idx, int val, int n, long[] prefix)
+//     {
+//         if(idx >= 30)
+//             return false;
+//         if(val > n)
+//             return false;
+//         if(val == n)
+//             return true;
 
-        boolean nottake = solve(idx+1, val, n, prefix);
-        boolean take = solve(idx + 1, val + (int)prefix[idx], n, prefix);
+//         boolean nottake = solve(idx + 1, val, n, prefix);
+//         boolean take = solve(idx + 1, val + (int)prefix[idx], n, prefix);
 
-        return nottake || take;
-    }
+//         return nottake || take;
+//     }
 
-    public boolean checkPowersOfThree(int n) {
-        long[] prefix = new long[30];
-        prefix[0] = 1;
+//     public boolean checkPowersOfThree(int n) {
+//         long[] prefix = new long[30];
+//         prefix[0] = 1;
 
-        for(int i = 1; i < 30; i++)
-        {
-            prefix[i] = prefix[i-1]*3;
-        }
+//         for(int i = 1; i < 30; i++)
+//         {
+//             prefix[i] = prefix[i-1]*3;
+//         }
         
-        return solve(0, 0, n, prefix);
+//         return solve(0, 0, n, prefix);
+//     }
+// }
+
+
+class Solution {
+    public boolean checkPowersOfThree(int n) {
+        while(n > 0)
+        {
+            if(n % 3 == 2)
+                return false;
+            n = n/3;
+        }
+        return true;
     }
 }
