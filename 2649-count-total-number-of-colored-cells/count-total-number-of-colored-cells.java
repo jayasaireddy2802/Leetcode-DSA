15864cc362233638d85587e65846fff07d17a8df
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-03-05 10:55:03 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (37.30%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
@@ -1,15 +1,26 @@
+// class Solution {
+//     public long coloredCells(int n) {
+//         long ans = n*2 - 1;
+//         long x = ans;
+
+//         while(x > 0)
+//         {
+//             x = x - 2;
+//             if(x > 0)
+//                 ans = ans + x*2;
+//         }
+
+//         return ans;
+//     }
+// }
+
+
+//progression formula
 class Solution {
     public long coloredCells(int n) {
-        long ans = n*2 - 1;
-        long x = ans;
-
-        while(x > 0)
-        {
-            x = x - 2;
-            if(x > 0)
-                ans = ans + x*2;
-        }
-
-        return ans;
+        long nth_term = n*2 - 1;
+        long sum_of_n_terms = ((1 + nth_term)*n) / 2;
+        long sum_of_second_last_terms = ((1 + nth_term - 2)*(n-1)) / 2;
+        return sum_of_n_terms + sum_of_second_last_terms;
     }
 }
