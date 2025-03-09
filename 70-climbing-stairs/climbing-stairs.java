a65c1879aa689bec2bb2eed990fd292353e3d6cb
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-03-09 22:07:31 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (17.91%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -42,27 +42,44 @@
 
 
 
+// class Solution {
+
+//     public int solve(int n, int[] dp)
+//     {
+//         if(n == 0 || n == 1)
+//             return 1;
+
+//         if(dp[n] != -1)
+//             return dp[n];
+
+//         int left = solve(n-1, dp);
+//         int right = solve(n-2, dp);
+
+//         return dp[n] = left + right;
+//     }
+
+//     public int climbStairs(int n) {
+        
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp, -1);
+//         return solve(n, dp);
+//     }
+// }
+
+ 
 class Solution {
-
-    public int solve(int n, int[] dp)
-    {
-        if(n == 0 || n == 1)
-            return 1;
-
-        if(dp[n] != -1)
-            return dp[n];
-
-        int left = solve(n-1, dp);
-        int right = solve(n-2, dp);
-
-        return dp[n] = left + right;
-    }
-
     public int climbStairs(int n) {
         
         int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return solve(n, dp);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 }
 
