c344af123be98aba8e4125938e96ce1ddadf88cf
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-03-11 13:18:30 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (35.77%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -1,45 +1,71 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode() {}
+//  *     ListNode(int val) { this.val = val; }
+//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  * }
+//  */
+// class Solution {
+//     public ListNode oddEvenList(ListNode head) {
+//         ListNode curr = head;
+//         ListNode evenHead = new ListNode(-1);
+//         ListNode oddHead = new ListNode(-1);
+//         ListNode even = evenHead;
+//         ListNode odd = oddHead;
+//         int flag = 1;
+
+//         while(curr != null)
+//         {
+//             if(flag == 0)
+//             {
+//                 ListNode temp = new ListNode(curr.val);
+//                 even.next = temp;
+//                 even = temp;
+//                 flag = 1;
+//             }
+//             else
+//             {
+//                 ListNode temp = new ListNode(curr.val);
+//                 odd.next = temp;
+//                 odd = temp;
+//                 flag = 0;
+//             }
+
+//             curr = curr.next;
+//         }
+
+//         oddHead = oddHead.next;
+//         odd.next = evenHead.next;
+
+//         return oddHead;
+//     }
+// }
+
+
+
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        ListNode curr = head;
-        ListNode evenHead = new ListNode(-1);
-        ListNode oddHead = new ListNode(-1);
-        ListNode even = evenHead;
-        ListNode odd = oddHead;
-        int flag = 1;
 
-        while(curr != null)
+        if(head == null || head.next == null)
+            return head;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while(even != null && even.next != null)
         {
-            if(flag == 0)
-            {
-                ListNode temp = new ListNode(curr.val);
-                even.next = temp;
-                even = temp;
-                flag = 1;
-            }
-            else
-            {
-                ListNode temp = new ListNode(curr.val);
-                odd.next = temp;
-                odd = temp;
-                flag = 0;
-            }
+            odd.next = odd.next.next;
+            even.next = even.next.next;
 
-            curr = curr.next;
+            odd = odd.next;
+            even = even.next;
         }
 
-        oddHead = oddHead.next;
-        odd.next = evenHead.next;
+        odd.next = evenHead;
+        return head;
 
-        return oddHead;
     }
 }
