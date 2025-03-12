e6e2223a2cb4203e38d54c7165ccdf3eff4e24fd
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-03-12 12:29:32 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (81.42%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -15,48 +15,48 @@
  */
 
 //  using recusion
-class Solution {
-    List<Integer> list;
-
-    public void preorder(TreeNode node)
-    {
-        if(node == null)
-            return ;
-        list.add(node.val);
-        preorder(node.left);
-        preorder(node.right);
-    }   
-
-    public List<Integer> preorderTraversal(TreeNode root) {
-        list = new ArrayList<>();
-        preorder(root);
-        return list;
-
-    }
-}
-
-// using iterative approach
 // class Solution {
-    
+//     List<Integer> list;
+
+//     public void preorder(TreeNode node)
+//     {
+//         if(node == null)
+//             return ;
+//         list.add(node.val);
+//         preorder(node.left);
+//         preorder(node.right);
+//     }   
+
 //     public List<Integer> preorderTraversal(TreeNode root) {
-//         List<Integer> list = new ArrayList<>();
-//         Stack<TreeNode> st = new Stack<>();
-//         if(root == null)
-//             return list;
-
-//         st.add(root);
-
-//         while(!st.isEmpty())
-//         {
-//             TreeNode node = st.pop();
-//             list.add(node.val);
-//             if(node.right != null)
-//                 st.add(node.right);
-//             if(node.left != null)
-//                 st.add(node.left);
-//         }
-
+//         list = new ArrayList<>();
+//         preorder(root);
 //         return list;
 
 //     }
 // }
+
+// using iterative approach
+class Solution {
+    
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        if(root == null)
+            return list;
+
+        st.add(root);
+
+        while(!st.isEmpty())
+        {
+            TreeNode node = st.pop();
+            list.add(node.val);
+            if(node.right != null)
+                st.add(node.right);
+            if(node.left != null)
+                st.add(node.left);
+        }
+
+        return list;
+
+    }
+}
