16797cc79569c91400176d7dad38afa48fb98396
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-03-14 19:24:57 +0530

    Time: 3 ms (82.47%) | Memory: 42.6 MB (66.99%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -14,7 +14,68 @@
  * }
  */
 
- class Pair
+//  class Pair
+//  {
+//     TreeNode node;
+//     int level;
+//     int data;
+//     int vertical;
+
+//     public Pair(TreeNode node, int level, int vertical, int data)
+//     {
+//         this.node = node;
+//         this.level = level;
+//         this.vertical = vertical;
+//         this.data = data;
+//     }
+//  }
+// class Solution {
+//     public List<List<Integer>> verticalTraversal(TreeNode root) {
+//         List<List<Integer>> ans = new ArrayList<>();
+//         Map<Integer, List<Pair>> map = new TreeMap<>();
+//         Queue<Pair> queue = new LinkedList<>();
+
+//         if(root == null)
+//             return ans;
+        
+//         queue.add(new Pair(root, 0, 0, root.val));
+
+//         while(!queue.isEmpty())
+//         {
+//             Pair pair = queue.poll();
+//             TreeNode node = pair.node;
+//             int level = pair.level;
+//             int vertical = pair.vertical;
+
+//             List<Pair> list = map.getOrDefault(vertical, new ArrayList<>());
+//             list.add(pair);
+//             map.put(vertical, list);
+
+//             if(node.left != null)
+//                 queue.add(new Pair(node.left, level + 1, vertical - 1, node.left.val));
+//             if(node.right != null)
+//                 queue.add(new Pair(node.right, level + 1, vertical + 1, node.right.val));
+//         }
+
+//         for(int key : map.keySet())
+//         {
+//             List<Integer> l = new ArrayList<>();
+//             List<Pair> list = map.get(key);
+//             Collections.sort(list, (a, b) -> (a.level == b.level) ? a.data - b.data : a.level - b.level);
+//             for(Pair node : list)
+//             {
+//                 l.add(node.data);
+//             }
+//             ans.add(l);            
+//         }
+
+//         return ans;
+//     }
+// }
+
+
+
+class Pair
  {
     TreeNode node;
     int level;
@@ -32,8 +93,10 @@
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
-        Map<Integer, List<Pair>> map = new TreeMap<>();
+        Map<Integer, List<Pair>> map = new HashMap<>();
         Queue<Pair> queue = new LinkedList<>();
+        int mini = 0;
+        int maxi = Integer.MIN_VALUE;
 
         if(root == null)
             return ans;
@@ -51,13 +114,17 @@ class Solution {
             list.add(pair);
             map.put(vertical, list);
 
-            if(node.left != null)
+            if(node.left != null){
                 queue.add(new Pair(node.left, level + 1, vertical - 1, node.left.val));
-            if(node.right != null)
+                mini = Math.min(mini, vertical - 1);
+            }
+            if(node.right != null){
                 queue.add(new Pair(node.right, level + 1, vertical + 1, node.right.val));
+                maxi = Math.max(maxi, vertical + 1);
+            }
         }
 
-        for(int key : map.keySet())
+        for(int key = mini; key <= maxi; key++)
         {
             List<Integer> l = new ArrayList<>();
             List<Pair> list = map.get(key);
