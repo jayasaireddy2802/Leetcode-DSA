544d3e41f30501bd02d35ea47d5fcbfe92e68661
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-03-15 16:44:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (62.06%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -14,6 +14,51 @@
  * }
  */
 
+// class Pair{
+//     TreeNode node;
+//     int level;
+
+//     public Pair(TreeNode node, int level)
+//     {
+//         this.node = node;
+//         this.level = level;
+//     }
+// }
+// class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         if(root == null)
+//             return ans;
+//         Queue<Pair> queue = new LinkedList<>();
+//         Map<Integer, Integer> map = new HashMap<>();
+//         int maxi = 0;
+
+//         queue.add(new Pair(root, 0));
+
+//         while(!queue.isEmpty())
+//         {
+//             Pair pair = queue.poll();
+//             TreeNode node = pair.node;
+//             int level = pair.level;
+
+//             map.put(level, node.val);
+
+//             if(node.left != null)
+//                 queue.add(new Pair(node.left, level + 1));
+//             if(node.right != null)
+//                 queue.add(new Pair(node.right, level + 1));
+
+//             maxi = Math.max(maxi, level + 1);
+//         }
+
+//         for(int key = 0; key < maxi; key++)
+//             ans.add(map.get(key));
+        
+//         return ans;
+//     }
+// }
+
+
 class Pair{
     TreeNode node;
     int level;
@@ -25,35 +70,22 @@ class Pair{
     }
 }
 class Solution {
+
+    public void dfs(TreeNode node, List<Integer> ans, int height)
+    {
+        if(node == null)
+            return ;
+        if(height == ans.size())
+            ans.add(node.val);
+        
+        dfs(node.right, ans, height + 1);
+        dfs(node.left, ans, height + 1);
+    }
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
         if(root == null)
             return ans;
-        Queue<Pair> queue = new LinkedList<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        int maxi = 0;
-
-        queue.add(new Pair(root, 0));
-
-        while(!queue.isEmpty())
-        {
-            Pair pair = queue.poll();
-            TreeNode node = pair.node;
-            int level = pair.level;
-
-            map.put(level, node.val);
-
-            if(node.left != null)
-                queue.add(new Pair(node.left, level + 1));
-            if(node.right != null)
-                queue.add(new Pair(node.right, level + 1));
-
-            maxi = Math.max(maxi, level + 1);
-        }
-
-        for(int key = 0; key < maxi; key++)
-            ans.add(map.get(key));
-        
+        dfs(root, ans, 0);
         return ans;
     }
 }
