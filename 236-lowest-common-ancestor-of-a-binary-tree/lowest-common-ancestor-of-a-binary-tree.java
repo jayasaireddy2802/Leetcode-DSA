06cab15f621a8dea27d1a1c8ca9ca34eef6add34
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-03-19 20:00:26 +0530

    Time: 6 ms (100.00%) | Memory: 44.3 MB (98.68%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -7,59 +7,79 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+// class Solution {
+
+//     public boolean traverse(TreeNode node, TreeNode p, List<TreeNode> list)
+//     {
+//         if(node == null)
+//             return false;
+        
+//         if(node == p)
+//         {
+//             list.add(node);
+//             return true;
+//         }
+
+//         list.add(node);
+//         boolean left = traverse(node.left, p, list);
+//         if(left)
+//             return left;
+//         boolean right = traverse(node.right, p, list);
+//         if(right)
+//             return right;
+        
+//         list.remove(list.size() - 1);
+        
+//         return false;
+//     }
+
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         List<TreeNode> path1 = new ArrayList<>();
+//         List<TreeNode> path2 = new ArrayList<>();
+        
+//         traverse(root, p, path1);
+//         traverse(root, q, path2);
+
+//         int i = 0, j = 0;
+//         int m = path1.size(), n = path2.size();
+//         TreeNode ans = null;
+//         while(i < m && j < n)
+//         {
+//             if(path1.get(i) == path2.get(j))
+//             {
+//                 ans = path1.get(i);
+//                 i++;
+//                 j++;
+//             }
+//             else
+//                 break;
+//         }
+
+//         return ans;
+//     }
+// }
+
 class Solution {
-
-    public boolean traverse(TreeNode node, TreeNode p, List<TreeNode> list)
-    {
-        if(node == null)
-            return false;
-        
-        if(node == p)
-        {
-            list.add(node);
-            return true;
-        }
-
-        list.add(node);
-        boolean left = traverse(node.left, p, list);
-        if(left)
-            return left;
-        boolean right = traverse(node.right, p, list);
-        if(right)
-            return right;
-        
-        list.remove(list.size() - 1);
-        
-        return false;
-    }
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode> path1 = new ArrayList<>();
-        List<TreeNode> path2 = new ArrayList<>();
+
+        if(root == null)
+            return null;
         
-        traverse(root, p, path1);
-        traverse(root, q, path2);
+        if(root == p)
+            return p;
+        
+        if(root == q)
+            return q;
+        
+        TreeNode left = lowestCommonAncestor(root.left, p, q); 
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-        for(TreeNode x : path1)
-        System.out.println(x.val);
-        for(TreeNode x : path2)
-        System.out.println(x.val);
+        if(left != null && right != null)
+            return root;
+        else if(left != null)
+            return left;
+        else
+            return right;
 
-        int i = 0, j = 0;
-        int m = path1.size(), n = path2.size();
-        TreeNode ans = null;
-        while(i < m && j < n)
-        {
-            if(path1.get(i) == path2.get(j))
-            {
-                ans = path1.get(i);
-                i++;
-                j++;
-            }
-            else
-                break;
-        }
-
-        return ans;
     }
 }
