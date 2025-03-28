e89edd19bd3756d4558a0e8c70dcce2904d5c3ed
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-03-28 22:42:39 +0530

    Time: 77 ms (76.43%) | Memory: 68.2 MB (95.17%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
@@ -13,6 +13,89 @@
  *     }
  * }
  */
+// class Solution {
+//     public int amountOfTime(TreeNode root, int start) {
+//         Map<TreeNode, TreeNode> map = new HashMap<>();
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         Set<TreeNode> visited = new HashSet<>();
+
+//         queue.add(root);
+//         map.put(root, null);
+
+//         while(!queue.isEmpty())
+//         {
+//             TreeNode node = queue.poll();
+//             if(node.left != null)
+//             {
+//                 queue.add(node.left);
+//                 map.put(node.left, node);
+//             }
+//             if(node.right != null)
+//             {
+//                 queue.add(node.right);
+//                 map.put(node.right, node);
+//             }
+//         }
+
+//         queue.add(root);
+//         TreeNode target = null;
+
+//         while(!queue.isEmpty())
+//         {
+//             TreeNode node = queue.poll();
+//             if(node.val == start){
+//                 target = node;
+//                 break;
+//             }
+//             if(node.left != null)
+//             {
+//                 queue.add(node.left);
+//             }
+//             if(node.right != null)
+//             {
+//                 queue.add(node.right);
+//             }
+//         }
+
+//         queue.clear();
+//         queue.add(target);
+//         visited.add(target);
+
+//         int time = -1;
+    
+//         while(!queue.isEmpty())
+//         {
+//             int size = queue.size();
+//             time++;
+
+//             for(int i = 0; i < size; i++)
+//             {
+//                 TreeNode node = queue.poll();
+//                 TreeNode parent = map.get(node);
+
+//                 if(node.left != null && (!visited.contains(node.left)))
+//                 {
+//                     queue.add(node.left);
+//                     visited.add(node.left);
+//                 }
+//                 if(node.right != null && (!visited.contains(node.right)))
+//                 {
+//                     queue.add(node.right);
+//                     visited.add(node.right);
+//                 }
+//                 if(parent != null && (!visited.contains(parent)))
+//                 {
+//                     queue.add(parent);
+//                     visited.add(parent);
+//                 }
+//             }
+
+//         }
+
+//         return time;
+//     }
+// }
+
 class Solution {
     public int amountOfTime(TreeNode root, int start) {
         Map<TreeNode, TreeNode> map = new HashMap<>();
@@ -21,10 +104,15 @@ class Solution {
 
         queue.add(root);
         map.put(root, null);
+        TreeNode target = null;
 
         while(!queue.isEmpty())
         {
             TreeNode node = queue.poll();
+            if(node.val == start)
+            {
+                target = node;
+            }
             if(node.left != null)
             {
                 queue.add(node.left);
@@ -37,27 +125,6 @@ class Solution {
             }
         }
 
-        queue.add(root);
-        TreeNode target = null;
-
-        while(!queue.isEmpty())
-        {
-            TreeNode node = queue.poll();
-            if(node.val == start){
-                target = node;
-                break;
-            }
-            if(node.left != null)
-            {
-                queue.add(node.left);
-            }
-            if(node.right != null)
-            {
-                queue.add(node.right);
-            }
-        }
-
-        queue.clear();
         queue.add(target);
         visited.add(target);
 
