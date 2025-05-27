3aca4406362c544233d1a9ac3ee31eb6c53d5148
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-05-27 11:51:51 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (82.55%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
@@ -1,16 +1,28 @@
+// class Solution {
+//     public int differenceOfSums(int n, int m) {
+//         int divisibleSum = 0;
+//         int notDivisibleSum = 0;
+
+//         for(int i = 1; i <= n; i++)
+//         {
+//             if(i % m == 0)
+//                 divisibleSum += i;
+//             else
+//                 notDivisibleSum += i;
+//         }
+
+//         return notDivisibleSum - divisibleSum;
+//     }
+// }
+
 class Solution {
     public int differenceOfSums(int n, int m) {
-        int divisibleSum = 0;
-        int notDivisibleSum = 0;
 
-        for(int i = 1; i <= n; i++)
-        {
-            if(i % m == 0)
-                divisibleSum += i;
-            else
-                notDivisibleSum += i;
-        }
+        int sumOfNumbersTillN = (n*(n+1)) / 2;
+        int terms = n / m;
+        int divisibleSum = m * terms * (terms + 1) / 2;
 
-        return notDivisibleSum - divisibleSum;
+        return sumOfNumbersTillN - (2 * divisibleSum);
+
     }
 }
