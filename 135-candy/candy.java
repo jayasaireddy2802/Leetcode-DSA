b8185b77eb53a6c16e6d0d7e3898b5231fb21bac
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-06-02 20:34:14 +0530

    Time: 3 ms (84.79%) | Memory: 45.6 MB (80.33%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -3,20 +3,26 @@ class Solution {
         int len = ratings.length;
         int[] leftToRight = new int[len];
         int[] rightToLeft = new int[len];
-        Arrays.fill(leftToRight, 1);
-        Arrays.fill(rightToLeft, 1);
+        // Arrays.fill(leftToRight, 1);
+        // Arrays.fill(rightToLeft, 1);
         int ans = 0;
+        leftToRight[0] = 1;
+        rightToLeft[len-1] = 1;
 
         for(int i = 1; i < len; i++)
         {
             if(ratings[i] > ratings[i-1])
                 leftToRight[i] = leftToRight[i - 1] + 1;
+            else
+                leftToRight[i] = 1;
         }
 
         for(int i = len - 2; i >= 0; i--)
         {
             if(ratings[i] > ratings[i+1])
                 rightToLeft[i] = rightToLeft[i + 1] + 1;
+            else
+                rightToLeft[i] = 1;
         }
 
         for(int i = 0; i < len; i++)
