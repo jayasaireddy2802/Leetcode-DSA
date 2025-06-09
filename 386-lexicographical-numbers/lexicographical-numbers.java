4b4321832f49871653cf7dd03052980110503147
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-06-09 12:51:56 +0530

    Time: 4 ms (71.48%) | Memory: 48.3 MB (69.42%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -1,8 +1,40 @@
+// class Solution {
+
+//     public void solve(String str, int n, List<Integer> ans)
+//     {
+//         int num = Integer.parseInt(str);
+//         if(num > n)
+//             return ;
+
+//         if(num <= n)
+//             ans.add(num);
+
+//         for(int i = 0; i <= 9; i++)
+//         {
+//             String s = str + Integer.toString(i);
+//             solve(s, n, ans);
+//         }
+        
+//     }
+
+//     public List<Integer> lexicalOrder(int n) {
+//         List<Integer> ans = new ArrayList<>();
+        
+//         for(int i = 1; i <= 9; i++)
+//         {
+//             String str = Integer.toString(i);
+//             solve(str, n, ans);
+//         }
+
+//         return ans;
+//     }
+// }
+
+
 class Solution {
 
-    public void solve(String str, int n, List<Integer> ans)
-    {
-        int num = Integer.parseInt(str);
+    public void solve(int num, int n, List<Integer> ans)
+    {        
         if(num > n)
             return ;
 
@@ -11,8 +43,8 @@ class Solution {
 
         for(int i = 0; i <= 9; i++)
         {
-            String s = str + Integer.toString(i);
-            solve(s, n, ans);
+            int newNum = num*10 + i;
+            solve(newNum, n, ans);
         }
         
     }
@@ -22,8 +54,7 @@ class Solution {
         
         for(int i = 1; i <= 9; i++)
         {
-            String str = Integer.toString(i);
-            solve(str, n, ans);
+            solve(i, n, ans);
         }
 
         return ans;
