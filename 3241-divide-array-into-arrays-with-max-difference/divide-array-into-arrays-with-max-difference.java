d0ea947d0c99167ddc9716a034f0073269f0ebfe
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-06-18 12:01:05 +0530

    Time: 23 ms (81.15%) | Memory: 58.5 MB (87.96%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
@@ -1,3 +1,30 @@
+// class Solution {
+//     public int[][] divideArray(int[] nums, int k) {
+//         Arrays.sort(nums);
+//         int len = nums.length;
+//         int[][] ans = new int[len/3][3];
+//         for(int i = 0; i < len - 2; i = i + 3)
+//         {
+//             if(nums[i+2] - nums[i] > k)
+//                 return new int[0][];
+//         }
+
+//         int j = 0;
+//         int k1 = 0;
+//         for(int i = 0; i < len/3; i++)
+//         {
+//             while(j < 3)
+//             {
+//                 ans[i][j] = nums[k1];
+//                 j++;
+//                 k1++;
+//             }
+//             j = 0;
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int[][] divideArray(int[] nums, int k) {
         Arrays.sort(nums);
@@ -7,19 +34,9 @@ class Solution {
         {
             if(nums[i+2] - nums[i] > k)
                 return new int[0][];
-        }
-
-        int j = 0;
-        int k1 = 0;
-        for(int i = 0; i < len/3; i++)
-        {
-            while(j < 3)
-            {
-                ans[i][j] = nums[k1];
-                j++;
-                k1++;
-            }
-            j = 0;
+            ans[i/3][0] = nums[i];
+            ans[i/3][1] = nums[i+1];
+            ans[i/3][2] = nums[i+2];
         }
         return ans;
     }
