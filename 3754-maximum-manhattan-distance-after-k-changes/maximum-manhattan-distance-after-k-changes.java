133fe4b97b092a486791c1fe0f11325c07296d9c
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-06-20 13:00:19 +0530

    Time: 147 ms (50.00%) | Memory: 45.8 MB (69.70%) - LeetSync

--- 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.java
+++ 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.java
@@ -1,45 +1,9 @@
-// class Solution {
-//     public int maxDistance(String s, int k) {
-//         int ans = 0;
-//         char[] hori = {'E', 'W'};
-//         char[] ver = {'N', 'S'};
-//         int len = s.length();
-
-//         for(char x : ver)
-//         {
-//             for(char y : hori)
-//             {
-//                 int val = 0;
-//                 int remaining = k;
-//                 for(int i = 0; i < len; i++)
-//                 {
-//                     char ch = s.charAt(i);
-//                     if(ch == x || ch == y)
-//                     {
-//                         val++;
-//                     }
-//                     else if(remaining > 0)
-//                     {
-//                         val++;
-//                         remaining--;
-//                     }
-//                     else
-//                         val--;
-
-//                     ans = Math.max(val, ans);
-//                 }
-//             }
-//         }
-
-//         return ans;
-//     }
-// }
-
 class Solution {
     public int maxDistance(String s, int k) {
         int ans = 0;
         char[] hori = {'E', 'W'};
         char[] ver = {'N', 'S'};
+        int len = s.length();
 
         for(char x : ver)
         {
@@ -47,8 +11,9 @@ class Solution {
             {
                 int val = 0;
                 int remaining = k;
-                for(char ch : s.toCharArray())
+                for(int i = 0; i < len; i++)
                 {
+                    char ch = s.charAt(i);
                     if(ch == x || ch == y)
                     {
                         val++;
@@ -69,3 +34,38 @@ class Solution {
         return ans;
     }
 }
+
+// class Solution {
+//     public int maxDistance(String s, int k) {
+//         int ans = 0;
+//         char[] hori = {'E', 'W'};
+//         char[] ver = {'N', 'S'};
+
+//         for(char x : ver)
+//         {
+//             for(char y : hori)
+//             {
+//                 int val = 0;
+//                 int remaining = k;
+//                 for(char ch : s.toCharArray())
+//                 {
+//                     if(ch == x || ch == y)
+//                     {
+//                         val++;
+//                     }
+//                     else if(remaining > 0)
+//                     {
+//                         val++;
+//                         remaining--;
+//                     }
+//                     else
+//                         val--;
+
+//                     ans = Math.max(val, ans);
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
