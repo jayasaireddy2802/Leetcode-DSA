7888280f56a4fe36761a950c415032c46a925790
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-06-20 12:59:52 +0530

    Time: 125 ms (53.03%) | Memory: 45.7 MB (81.82%) - LeetSync

--- 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.java
+++ 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.java
@@ -1,9 +1,45 @@
+// class Solution {
+//     public int maxDistance(String s, int k) {
+//         int ans = 0;
+//         char[] hori = {'E', 'W'};
+//         char[] ver = {'N', 'S'};
+//         int len = s.length();
+
+//         for(char x : ver)
+//         {
+//             for(char y : hori)
+//             {
+//                 int val = 0;
+//                 int remaining = k;
+//                 for(int i = 0; i < len; i++)
+//                 {
+//                     char ch = s.charAt(i);
+//                     if(ch == x || ch == y)
+//                     {
+//                         val++;
+//                     }
+//                     else if(remaining > 0)
+//                     {
+//                         val++;
+//                         remaining--;
+//                     }
+//                     else
+//                         val--;
+
+//                     ans = Math.max(val, ans);
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int maxDistance(String s, int k) {
         int ans = 0;
         char[] hori = {'E', 'W'};
         char[] ver = {'N', 'S'};
-        int len = s.length();
 
         for(char x : ver)
         {
@@ -11,9 +47,8 @@ class Solution {
             {
                 int val = 0;
                 int remaining = k;
-                for(int i = 0; i < len; i++)
+                for(char ch : s.toCharArray())
                 {
-                    char ch = s.charAt(i);
                     if(ch == x || ch == y)
                     {
                         val++;
