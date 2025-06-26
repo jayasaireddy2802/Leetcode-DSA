18d196d4d4140a0fbb17824aa1b16784bfe695aa
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-06-26 17:26:36 +0530

    Time: 1 ms (100.00%) | Memory: 42.1 MB (21.28%) - LeetSync

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.java
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.java
@@ -1,14 +1,43 @@
+// class Solution {
+//     public int longestSubsequence(String s, int k) {
+//         int len = s.length();
+//         int ct = 0;
+
+//         for(int i = 0; i < len; i++)
+//         {
+//             if(s.charAt(i) == '0')
+//                 ct++;
+//         }
+
+//         long num2 = 1;
+//         long res = 0;
+
+//         for(int i = len -1; i >= 0; i--)
+//         {
+//             if(s.charAt(i) == '1')
+//             {
+//                 if(res + num2 <= k)
+//                 {
+//                     ct++;
+//                     res = res + num2;
+//                 }
+
+//             }
+
+//             num2 = num2 << 1;
+//             if(num2 > k)
+//                 break;
+//         }
+
+//         return ct;
+//     }
+// }
+
 class Solution {
     public int longestSubsequence(String s, int k) {
         int len = s.length();
         int ct = 0;
-
-        for(int i = 0; i < len; i++)
-        {
-            if(s.charAt(i) == '0')
-                ct++;
-        }
-
+        boolean flag = false;
         long num2 = 1;
         long res = 0;
 
@@ -16,17 +45,20 @@ class Solution {
         {
             if(s.charAt(i) == '1')
             {
-                if(res + num2 <= k)
+                if(res + num2 <= k && (!flag))
                 {
                     ct++;
                     res = res + num2;
                 }
-
+            }
+            else
+            {
+                ct++;
             }
 
             num2 = num2 << 1;
             if(num2 > k)
-                break;
+                flag = true;            
         }
 
         return ct;
