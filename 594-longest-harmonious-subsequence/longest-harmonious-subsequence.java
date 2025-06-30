26369dcafd4074db9fc1aef0ba0cd3ae21988ba5
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-06-30 10:44:01 +0530

    Time: 16 ms (62.12%) | Memory: 45.4 MB (87.59%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
@@ -1,27 +1,46 @@
+// class Solution {
+//     public int findLHS(int[] nums) {
+
+//         int len = nums.length;
+//         int ans = 0;
+
+//         for(int i = 0; i < len; i++)
+//         {
+//             int l = 0;
+//             int min = Integer.MAX_VALUE;
+//             int max = Integer.MIN_VALUE;
+
+//             for(int j = 0; j < len; j++)
+//             {
+//                 if(nums[i] == nums[j] || (nums[i] - nums[j] == 1))
+//                 {
+//                     max = Math.max(max, nums[j]);
+//                     min = Math.min(min, nums[j]);
+//                     l++;
+//                 }
+//             }
+//             if(max - min == 1)
+//                 ans = Math.max(ans, l);
+//         }    
+
+//         return ans;
+//     }
+// }
+
+
 class Solution {
     public int findLHS(int[] nums) {
-
-        int len = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
         int ans = 0;
 
-        for(int i = 0; i < len; i++)
+        for(int num : nums)
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        
+        for(int key : map.keySet())
         {
-            int l = 0;
-            int min = Integer.MAX_VALUE;
-            int max = Integer.MIN_VALUE;
-
-            for(int j = 0; j < len; j++)
-            {
-                if(nums[i] == nums[j] || (nums[i] - nums[j] == 1))
-                {
-                    max = Math.max(max, nums[j]);
-                    min = Math.min(min, nums[j]);
-                    l++;
-                }
-            }
-            if(max - min == 1)
-                ans = Math.max(ans, l);
-        }    
+            if(map.containsKey(key + 1))
+                ans = Math.max(ans, map.get(key) + map.get(key + 1));
+        }
 
         return ans;
     }
