6e98010cfb6708b3b490526fe7561d26271c3208
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-07-01 11:40:18 +0530

    Time: 1 ms (87.19%) | Memory: 42.3 MB (26.45%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
@@ -1,12 +1,31 @@
+// class Solution {
+//     public int possibleStringCount(String word) {
+//         int ct = 1;
+//         int len = word.length();
+
+//         for(int i = 1; i < len; i++)
+//         {   
+//             if(word.charAt(i) == word.charAt(i-1))
+//                 ct++;
+//         }
+
+//         return ct;
+//     }
+// }
+
+
 class Solution {
     public int possibleStringCount(String word) {
         int ct = 1;
         int len = word.length();
+        char ch = word.charAt(0);
 
         for(int i = 1; i < len; i++)
         {   
-            if(word.charAt(i) == word.charAt(i-1))
+            if(ch == word.charAt(i))
                 ct++;
+            else
+                ch = word.charAt(i);
         }
 
         return ct;
