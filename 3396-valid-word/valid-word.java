2201abef513440ce9ec50c6cef4c356900daaf62
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-07-15 11:30:04 +0530

    Time: 1 ms (99.12%) | Memory: 42.3 MB (26.49%) - LeetSync

--- 3396-valid-word/valid-word.java
+++ 3396-valid-word/valid-word.java
@@ -1,38 +1,63 @@
+// class Solution {
+//     public boolean isValid(String word) {
+//         int len = word.length();
+//         if(len < 3)
+//             return false;
+
+//         boolean anyVowel = false;
+//         boolean anyConsonant = false;
+
+//         for(int i = 0; i < len; i++)
+//         {
+//             char ch = word.charAt(i);
+
+//             if((ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+//             {
+//                 if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+//                 {
+//                     anyVowel = true;
+//                 }
+//                 else
+//                 {
+//                     if(ch >= '0' && ch <= '9')
+//                         continue;
+//                     else
+//                         anyConsonant = true;
+//                 }
+//             }
+
+//             else
+//             {
+//                 return false;
+//             }
+//         }
+
+//         return (anyVowel && anyConsonant);
+
+//     }
+// }
+
+
 class Solution {
     public boolean isValid(String word) {
-        int len = word.length();
-        if(len < 3)
-            return false;
+        if (word.length() < 3) return false;
 
-        boolean anyVowel = false;
-        boolean anyConsonant = false;
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
 
-        for(int i = 0; i < len; i++)
-        {
-            char ch = word.charAt(i);
+        for (char ch : word.toCharArray()) {
+            if (!Character.isLetterOrDigit(ch)) return false;
 
-            if((ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
-            {
-                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
-                {
-                    anyVowel = true;
+            if (Character.isLetter(ch)) {
+                char lower = Character.toLowerCase(ch);
+                if ("aeiou".indexOf(lower) != -1) {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
                 }
-                else
-                {
-                    if(ch >= '0' && ch <= '9')
-                        continue;
-                    else
-                        anyConsonant = true;
-                }
-            }
-
-            else
-            {
-                return false;
             }
         }
 
-        return (anyVowel && anyConsonant);
-
+        return hasVowel && hasConsonant;
     }
 }
