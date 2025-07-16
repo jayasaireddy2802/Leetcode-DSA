e8e63a3769f19a4b38be7a6ab6c4fca24be599b8
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-07-16 12:45:48 +0530

    Time: 12 ms (22.13%) | Memory: 62.2 MB (74.59%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.java
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.java
@@ -1,3 +1,95 @@
+// class Solution {
+//     public int maximumLength(int[] nums) {
+//         int ct = 0;
+//         int ans = 0;
+//         int len = nums.length;
+
+//         // odd even sequence
+//         int idx = 0;
+//         boolean odd = false;
+//         for(idx = 0; idx < len; idx++)
+//         {
+//             if(nums[idx] % 2 == 1)
+//                 break;
+//         }
+//         ct = 1;
+//         for(int i = idx; i < len; i++)
+//         {
+//             if(odd)
+//             {
+//                 if(nums[i] % 2 == 1)
+//                 {
+//                     ct++;
+//                     odd = false;
+//                 }
+//             }
+//             else
+//             {
+//                 if(nums[i] % 2 == 0)
+//                 {
+//                     ct++;
+//                     odd = true;
+//                 }
+//             }
+//         }
+//         ans = Math.max(ans, ct);
+
+//         // even odd sequence
+//         ct = 0;
+//         idx = 0;
+//         boolean even = false;
+//         for(idx = 0; idx < len; idx++)
+//         {
+//             if(nums[idx] % 2 == 0)
+//                 break;
+//         }
+//         ct = 1;
+//         for(int i = idx; i < len; i++)
+//         {
+//             if(even)
+//             {
+//                 if(nums[i] % 2 == 0)
+//                 {
+//                     ct++;
+//                     even = false;
+//                 }
+//             }
+//             else
+//             {
+//                 if(nums[i] % 2 == 1)
+//                 {
+//                     ct++;
+//                     even = true;
+//                 }
+//             }
+//         }
+//         ans = Math.max(ans, ct);
+
+//         // even even
+//         ct = 0;
+//         for(int num : nums)
+//         {
+//             if(num % 2 == 0)
+//                 ct++;
+//         }
+//         ans = Math.max(ans, ct);
+
+//         // odd odd
+//         ct = 0;
+//         for(int num : nums)
+//         {
+//             if(num % 2 == 1)
+//                 ct++;
+//         }
+//         ans = Math.max(ans, ct);
+
+
+//         return ans;
+//     }
+// }
+
+
+
 class Solution {
     public int maximumLength(int[] nums) {
         int ct = 0;
@@ -6,63 +98,18 @@ class Solution {
 
         // odd even sequence
         int idx = 0;
-        boolean odd = false;
-        for(idx = 0; idx < len; idx++)
-        {
-            if(nums[idx] % 2 == 1)
-                break;
-        }
         ct = 1;
-        for(int i = idx; i < len; i++)
+        int parity = nums[0] % 2;
+        for(idx = 1; idx < len; idx++)
         {
-            if(odd)
+            int res = nums[idx] % 2;
+            if(res != parity)
             {
-                if(nums[i] % 2 == 1)
-                {
-                    ct++;
-                    odd = false;
-                }
-            }
-            else
-            {
-                if(nums[i] % 2 == 0)
-                {
-                    ct++;
-                    odd = true;
-                }
+                ct++;
+                parity = res;
             }
         }
-        ans = Math.max(ans, ct);
-
-        // even odd sequence
-        ct = 0;
-        idx = 0;
-        boolean even = false;
-        for(idx = 0; idx < len; idx++)
-        {
-            if(nums[idx] % 2 == 0)
-                break;
-        }
-        ct = 1;
-        for(int i = idx; i < len; i++)
-        {
-            if(even)
-            {
-                if(nums[i] % 2 == 0)
-                {
-                    ct++;
-                    even = false;
-                }
-            }
-            else
-            {
-                if(nums[i] % 2 == 1)
-                {
-                    ct++;
-                    even = true;
-                }
-            }
-        }
+        
         ans = Math.max(ans, ct);
 
         // even even
