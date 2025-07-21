0c14451975c55f33f128c208f416b30532317ab1
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-07-21 12:17:19 +0530

    Time: 35 ms (57.83%) | Memory: 45.9 MB (40.66%) - LeetSync

---
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
@@ -0,0 +1,44 @@
+// class Solution {
+//     public String makeFancyString(String s) {
+//         String str = "";
+//         int i = 0, j = 0, ct = 1;
+//         int len = s.length();
+//         while(j < len)
+//         {
+//             if(j > 0 && s.charAt(j - 1) == s.charAt(j))
+//             {
+//                 ct++;
+//             }
+//             else
+//             {
+//                 ct = 1;
+//             }
+//             if(ct < 3)
+//                 str = str + s.charAt(j);
+//             j++;
+//         }
+//         return str;
+//     }
+// }
+
+
+class Solution {
+    public String makeFancyString(String s) {
+        StringBuilder sb = new StringBuilder();
+        int count = 1; // to count consecutive same characters
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (i > 0 && s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                count = 1;
+            }
+            
+            if (count < 3) {
+                sb.append(s.charAt(i));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
