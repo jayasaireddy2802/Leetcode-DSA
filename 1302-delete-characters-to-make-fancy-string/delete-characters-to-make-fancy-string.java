a36a66cc28ac00be637820953685c204a946811f
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-07-21 12:20:53 +0530

    Time: 32 ms (78.03%) | Memory: 46 MB (14.90%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
@@ -22,23 +22,48 @@
 // }
 
 
+// class Solution {
+//     public String makeFancyString(String s) {
+//         StringBuilder sb = new StringBuilder();
+//         int count = 1; // to count consecutive same characters
+        
+//         for (int i = 0; i < s.length(); i++) {
+//             if (i > 0 && s.charAt(i) == s.charAt(i - 1)) {
+//                 count++;
+//             } else {
+//                 count = 1;
+//             }
+            
+//             if (count < 3) {
+//                 sb.append(s.charAt(i));
+//             }
+//         }
+        
+//         return sb.toString();
+//     }
+// }
+
 class Solution {
     public String makeFancyString(String s) {
-        StringBuilder sb = new StringBuilder();
-        int count = 1; // to count consecutive same characters
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (i > 0 && s.charAt(i) == s.charAt(i - 1)) {
-                count++;
-            } else {
-                count = 1;
+        StringBuilder str = new StringBuilder();
+        int i = 0, j = 0, ct = 1;
+        int len = s.length();
+
+        while(j < len)
+        {
+            if(j > 0 && s.charAt(j - 1) == s.charAt(j))
+            {
+                ct++;
             }
-            
-            if (count < 3) {
-                sb.append(s.charAt(i));
+            else
+            {
+                ct = 1;
             }
+            if(ct < 3)
+                str = str.append(s.charAt(j));
+            j++;
         }
-        
-        return sb.toString();
+        return str.toString();
     }
 }
+
