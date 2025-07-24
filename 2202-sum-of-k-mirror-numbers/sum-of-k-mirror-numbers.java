b7673244a2b13181b94ee8543ea347520ce53f43
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-07-24 14:10:27 +0530

    Time: 943 ms (18.19%) | Memory: 107.5 MB (5.12%) - LeetSync

--- 2202-sum-of-k-mirror-numbers/sum-of-k-mirror-numbers.java
+++ 2202-sum-of-k-mirror-numbers/sum-of-k-mirror-numbers.java
@@ -32,6 +32,72 @@
 // }
 
 
+// class Solution {
+
+//     public boolean isPalindrome(String str)
+//     {
+
+//         int len = str.length();
+//         int i = 0;
+//         while(i < len / 2)
+//         {
+//             if(str.charAt(i) != str.charAt(len - i - 1))
+//                 return false;
+//             i++;
+//         }
+
+//         return true;
+//     }
+
+//     public List<Long> generatePalindromes(int length)
+//     {
+//         int half_length = (length + 1) / 2;
+//         List<Long> list = new ArrayList<>();
+//         long start = (long) Math.pow(10, half_length - 1);
+//         long end = (long) Math.pow(10, half_length);
+
+//         for(long i = start; i < end; i++)
+//         {
+//             String halfString = Long.toString(i);
+//             int j = length / 2;
+//             j--;
+//             while(j >= 0)
+//             {
+//                 halfString = halfString + halfString.charAt(j);
+//                 j--;
+//             }
+//             list.add(Long.parseLong(halfString));
+//         }
+
+//         return list;
+//     }
+
+//     public long kMirror(int k, int n) {
+
+//         int len = 50;
+//         int ct = 0;
+//         long ans = 0;
+
+//         for(int i = 1;  i < len; i++)
+//         {
+//             List<Long> list = generatePalindromes(i);
+//             for(long num : list)
+//             {
+//                 if(isPalindrome(Long.toString(num, k)))
+//                 {
+//                     ct++;
+//                     ans += num;
+//                 }
+//                 if(ct == n)
+//                     return ans;
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
+
+
 class Solution {
 
     public boolean isPalindrome(String str)
@@ -58,15 +124,15 @@ class Solution {
 
         for(long i = start; i < end; i++)
         {
-            String halfString = Long.toString(i);
+            StringBuilder halfString = new StringBuilder(Long.toString(i));
             int j = length / 2;
             j--;
             while(j >= 0)
             {
-                halfString = halfString + halfString.charAt(j);
+                halfString.append(halfString.charAt(j));
                 j--;
             }
-            list.add(Long.parseLong(halfString));
+            list.add(Long.parseLong(halfString.toString()));
         }
 
         return list;
