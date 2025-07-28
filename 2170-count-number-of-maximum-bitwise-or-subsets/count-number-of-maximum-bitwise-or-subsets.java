63657cb01966b1128cdbe3fd5b6b527a59b6048c
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-07-28 15:55:31 +0530

    Time: 9 ms (48.40%) | Memory: 41.1 MB (64.39%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,33 +1,63 @@
+// class Solution {
+
+//     public void generateSubsequence(int idx, int[] nums, int val, List<Integer> list)
+//     {
+//         if(idx >= nums.length)
+//         {
+//             list.add(val);
+//             return ;
+//         }
+
+//         generateSubsequence(idx + 1, nums, val | nums[idx], list);
+//         generateSubsequence(idx + 1, nums, val , list);
+//     }
+
+//     public int countMaxOrSubsets(int[] nums) {
+//         List<Integer> list = new ArrayList<>();
+
+//         generateSubsequence(0, nums, 0, list);
+
+//         int maxi = 0;
+//         int ans = 0;
+
+//         for(int num : list)
+//             maxi = Math.max(maxi, num);
+//         for(int num : list)
+//         {
+//             if(num == maxi)
+//                 ans++;
+//         }
+
+//         return ans;
+//     }
+// }
+
+
+
 class Solution {
 
-    public void generateSubsequence(int idx, int[] nums, int val, List<Integer> list)
+    public int generateSubsequence(int idx, int[] nums, int val, int maxi)
     {
         if(idx >= nums.length)
         {
-            list.add(val);
-            return ;
+            if(val == maxi)
+                return 1;
+            else
+                return 0;
         }
 
-        generateSubsequence(idx + 1, nums, val | nums[idx], list);
-        generateSubsequence(idx + 1, nums, val , list);
+        int l = generateSubsequence(idx + 1, nums, val | nums[idx], maxi);
+        int r = generateSubsequence(idx + 1, nums, val , maxi);
+
+        return l + r;
     }
 
     public int countMaxOrSubsets(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-
-        generateSubsequence(0, nums, 0, list);
-
         int maxi = 0;
-        int ans = 0;
+        for(int num : nums)
+            maxi = maxi | num;
 
-        for(int num : list)
-            maxi = Math.max(maxi, num);
-        for(int num : list)
-        {
-            if(num == maxi)
-                ans++;
-        }
+        return generateSubsequence(0, nums, 0, maxi);
 
-        return ans;
     }
 }
