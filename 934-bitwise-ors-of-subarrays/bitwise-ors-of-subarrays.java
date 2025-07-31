31de30e5f891bceeeeafd6e89aad29e2f901b42c
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-07-31 13:24:30 +0530

    Time: 119 ms (99.59%) | Memory: 71 MB (90.57%) - LeetSync

--- 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.java
+++ 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.java
@@ -20,22 +20,58 @@
 //     }
 // }
 
+// class Solution {
+//     public int subarrayBitwiseORs(int[] arr) {
+//         Set<Integer> set = new HashSet<>();
+//         Set<Integer> ans = new HashSet<>();
+
+//         for(int num : arr)
+//         {
+//             Set<Integer> curr = new HashSet<>();
+//             curr.add(num);
+//             for(int x : set)
+//                 curr.add(x | num);
+
+//             ans.addAll(curr);
+//             set = curr;           
+//         }
+        
+//         return ans.size();
+//     }
+// }
+
+// class Solution {
+//     public int subarrayBitwiseORs(int[] arr) {
+//         Set<Integer> set = new HashSet<>();
+        
+//         for(int i = 0; i < arr.length; i++)
+//         {
+//             set.add(arr[i]);
+//             int val = arr[i];
+
+//             for(int j = i + 1; j < arr.length; j++)
+//             {
+//                 if(val == (val | arr[j])) break;
+//                 val = val | arr[j];
+//                 set.add(val);
+//             }
+//         }
+        
+//         return set.size();
+//     }
+// }
+
 class Solution {
     public int subarrayBitwiseORs(int[] arr) {
-        Set<Integer> set = new HashSet<>();
-        Set<Integer> ans = new HashSet<>();
-
-        for(int num : arr)
-        {
-            Set<Integer> curr = new HashSet<>();
-            curr.add(num);
-            for(int x : set)
-                curr.add(x | num);
-
-            ans.addAll(curr);
-            set = curr;           
+        Set<Integer> result=new HashSet<>();
+        for(int i=0;i<arr.length;i++){
+            result.add(arr[i]);
+            for(int j=i-1;j>=0;j--){
+                if(arr[j]==(arr[j] | arr[i])) break;
+                arr[j]|=arr[i];
+                result.add(arr[j]);
+            }
         }
-        
-        return ans.size();
+        return result.size();
     }
 }
