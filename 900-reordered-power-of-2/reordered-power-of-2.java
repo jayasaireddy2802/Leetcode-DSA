28dd12d86fea05b6b556575214aa04c775c6da74
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-08-10 11:36:24 +0530

    Time: 1 ms (85.79%) | Memory: 41.4 MB (18.42%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.java
+++ 900-reordered-power-of-2/reordered-power-of-2.java
@@ -1,39 +1,64 @@
-class Solution {
+// class Solution {
 
-    public boolean powerOfTwo(int n) {
-        return (n & (n - 1)) == 0;
+//     public boolean powerOfTwo(int n) {
+//         return (n & (n - 1)) == 0;
+//     }
+
+//     public boolean solve(int idx, StringBuilder sb) {
+//         int len = sb.length();
+
+//         // If we've placed all characters, check the number
+//         if (idx == len) {
+//             if (sb.charAt(0) == '0') return false; // avoid leading zeros
+//             int n = Integer.parseInt(sb.toString());
+//             return powerOfTwo(n);
+//         }
+
+//         for (int i = idx; i < len; i++) {
+//             // swap idx and i
+//             char temp = sb.charAt(idx);
+//             sb.setCharAt(idx, sb.charAt(i));
+//             sb.setCharAt(i, temp);
+
+//             if (solve(idx + 1, sb)) {
+//                 return true;
+//             }
+
+//             // swap back
+//             temp = sb.charAt(idx);
+//             sb.setCharAt(idx, sb.charAt(i));
+//             sb.setCharAt(i, temp);
+//         }
+//         return false;
+//     }
+
+//     public boolean reorderedPowerOf2(int n) {
+//         StringBuilder sb = new StringBuilder(Integer.toString(n));
+//         return solve(0, sb);
+//     }
+// }
+
+class Solution{
+    public String sort(int n)
+    {
+        char[] arr = Integer.toString(n).toCharArray();
+        Arrays.sort(arr);
+        return new String(arr);
     }
-
-    public boolean solve(int idx, StringBuilder sb) {
-        int len = sb.length();
-
-        // If we've placed all characters, check the number
-        if (idx == len) {
-            if (sb.charAt(0) == '0') return false; // avoid leading zeros
-            int n = Integer.parseInt(sb.toString());
-            return powerOfTwo(n);
-        }
-
-        for (int i = idx; i < len; i++) {
-            // swap idx and i
-            char temp = sb.charAt(idx);
-            sb.setCharAt(idx, sb.charAt(i));
-            sb.setCharAt(i, temp);
-
-            if (solve(idx + 1, sb)) {
-                return true;
-            }
-
-            // swap back
-            temp = sb.charAt(idx);
-            sb.setCharAt(idx, sb.charAt(i));
-            sb.setCharAt(i, temp);
-        }
-        return false;
-    }
-
     public boolean reorderedPowerOf2(int n) {
-        StringBuilder sb = new StringBuilder(Integer.toString(n));
-        return solve(0, sb);
+        
+        String str = sort(n);
+
+        int num = 0;
+        int i = 0;
+        while(i < 31)
+        {
+            num = (1 << i);
+            if(str.equals(sort(num)))
+                return true;
+            i++;
+        }
+
+        return false;
     }
 }
