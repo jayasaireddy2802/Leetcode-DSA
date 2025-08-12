b10dc56c2a0326d5f55f830af48d110089e4913e
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-08-12 17:01:40 +0530

    Time: 16 ms (84.95%) | Memory: 92.8 MB (39.78%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.java
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.java
@@ -1,28 +1,128 @@
+// import java.util.*;
+
+// class Solution {
+//     int mod = 1000000007;
+
+//     private int[] powersOfTwo(int n) {
+//         List<Integer> list = new ArrayList<>();
+//         String binary = Integer.toString(n, 2);
+//         int len = binary.length();
+//         int ct = 0;
+
+//         for (int i = len - 1; i >= 0; i--) {
+//             if (binary.charAt(i) == '1')
+//                 list.add(1 << ct);
+//             ct++;
+//         }
+
+//         int[] powers = new int[list.size()];
+//         for (int i = 0; i < list.size(); i++) {
+//             powers[i] = list.get(i);
+//         }
+//         return powers;
+//     }
+
+//     private long modPow(long base, long exp) {
+//         long result = 1;
+//         while (exp > 0) {
+//             if ((exp & 1) == 1) result = (result * base) % mod;
+//             base = (base * base) % mod;
+//             exp >>= 1;
+//         }
+//         return result;
+//     }
+
+//     public int[] productQueries(int n, int[][] queries) {
+//         int[] powers = powersOfTwo(n);
+//         int m = powers.length;
+
+//         long[] prefix = new long[m];
+//         prefix[0] = powers[0] % mod;
+//         for (int i = 1; i < m; i++) {
+//             prefix[i] = (prefix[i - 1] * powers[i]) % mod;
+//         }
+
+//         int[] ans = new int[queries.length];
+//         for (int i = 0; i < queries.length; i++) {
+//             int x = queries[i][0];
+//             int y = queries[i][1];
+//             if (x == 0) {
+//                 ans[i] = (int) (prefix[y] % mod);
+//             } else {
+//                 long inv = modPow(prefix[x - 1], mod - 2); // modular inverse
+//                 ans[i] = (int) ((prefix[y] * inv) % mod);
+//             }
+//         }
+//         return ans;
+//     }
+// }
+// 
+
+
+
 class Solution {
-    private static final int MODULO = 1_000_000_007;
+    int mod = 1000000007;
+
+    private int[] powersOfTwo(int n)
+    {
+        List<Integer> list = new ArrayList<>();
+
+        String binary = Integer.toString(n, 2);
+        int len = binary.length();
+        int ct = 0;
+
+        for(int i = len - 1; i >= 0; i--)
+        {
+            if(binary.charAt(i) == '1')
+                list.add(1 << ct);
+            ct++;
+        }
+
+        len = list.size();
+        int powers[] = new int[len];
+        int i = 0;
+        for(int num : list)
+        {
+            powers[i] = num;
+            i++;
+        }
+
+        return powers;
+    }
+
+    private long modPow(long base, long exp) {
+        long result = 1;
+        while (exp > 0) {
+            if ((exp & 1) == 1) result = (result * base) % mod;
+            base = (base * base) % mod;
+            exp >>= 1;
+        }
+        return result;
+    }
 
     public int[] productQueries(int n, int[][] queries) {
-        List<Long> bitPowers = new ArrayList<>();
-        
-        // Extract powers of two from binary representation of n
-        for (int bit = 0; bit < 32; bit++) {
-            if (((n >> bit) & 1) == 1) {
-                bitPowers.add(1L << bit);
-            }
-        }
+        int len = queries.length;
 
-        int[] answers = new int[queries.length];
-        for (int q = 0; q < queries.length; q++) {
-            int left = queries[q][0];
-            int right = queries[q][1];
-            
+        int[] ans = new int[len];
+
+        int[] powers = powersOfTwo(n);
+
+        int j = 0;
+        for(int[] query : queries)
+        {
+            int x = query[0];
+            int y = query[1];
             long product = 1;
-            for (int idx = left; idx <= right; idx++) {
-                product = (product * bitPowers.get(idx)) % MODULO;
+
+            for(int i = x; i <= y; i++)
+            {
+                product = (product * powers[i]) % mod;
             }
-            answers[q] = (int) product;
+
+            ans[j] = (int)product;
+            j++;
         }
 
-        return answers;
+        return ans;
     }
 }
