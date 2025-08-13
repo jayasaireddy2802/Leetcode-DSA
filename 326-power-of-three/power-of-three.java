48256b524e24bf0abd9fbcb04e7753f3782023e1
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-08-13 18:35:07 +0530

    Time: 8 ms (84.41%) | Memory: 44.5 MB (10.93%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -18,11 +18,23 @@
 // }
 
 
+// class Solution {
+//     public boolean isPowerOfThree(int n) {
+
+//         double res = Math.log10(n)/Math.log10(3);
+//         return res == (int)res;
+
+//     }
+// }
+
+
+
 class Solution {
     public boolean isPowerOfThree(int n) {
-
-        double res = Math.log10(n)/Math.log10(3);
-        return res == (int)res;
+        if(n <= 0)
+            return false;
+        int res = (int)Math.pow(3, 19);
+        return res % n == 0;
 
     }
 }
