0c83a63ae2c39b3ceba485fec95dcb00ebc329d6
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-08-16 11:18:07 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (94.76%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -1,15 +1,34 @@
+// class Solution {
+//     public int maximum69Number (int num) {
+//         char[] arr = Integer.toString(num).toCharArray();
+//         int len = arr.length;
+//         for(int i = 0; i < len; i++)
+//         {
+//             if(arr[i] == '6')
+//             {
+//                 arr[i] = '9';
+//                 break;
+//             }
+//         }
+//         return Integer.parseInt(new String(arr));
+//     }
+// }
+
+
 class Solution {
     public int maximum69Number (int num) {
-        char[] arr = Integer.toString(num).toCharArray();
-        int len = arr.length;
-        for(int i = 0; i < len; i++)
+        int place = -1, temp = num, ct = 0;
+        
+        while(temp > 0)
         {
-            if(arr[i] == '6')
+            if(temp % 10 == 6)
             {
-                arr[i] = '9';
-                break;
+                place = ct;
             }
+            temp /= 10;
+            ct++;
         }
-        return Integer.parseInt(new String(arr));
+
+        return (place == -1) ? num : num + 3 * (int)Math.pow(10, place);
     }
 }
