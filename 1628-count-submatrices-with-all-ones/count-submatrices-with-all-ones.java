3f4ec04e85f784d7cde53f4c650d7b0dee72a9e1
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-08-21 17:12:37 +0530

    Time: 248 ms (5.14%) | Memory: 45.3 MB (83.56%) - LeetSync

---
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    private void fillArray(int x, int y, int[] arr, int[][] mat)
+    {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        for(int j = 0; j < n; j++)
+        {
+            int val = 1;
+            for(int i = x; i <= y; i++)
+            {
+                val = (val & mat[i][j]);
+            }
+            arr[j] = val;
+        }
+    }
+
+    private int count(int[] mat)
+    {
+        int ans = 0;
+        int ct = 0;
+        int len = mat.length;
+
+        for(int i = 0; i < len; i++)
+        {
+            if(mat[i] == 1)
+                ct++;
+            else
+                ct = 0;
+            ans = ans + ct;
+        }
+
+        return ans;
+    }
+
+    public int numSubmat(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int ans = 0;
+
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = i; j < m; j++)
+            {
+                int[] arr = new int[n];
+                fillArray(i, j, arr, mat);
+                ans = ans + count(arr);
+            }
+        }
+
+        return ans;
+    }
+}
