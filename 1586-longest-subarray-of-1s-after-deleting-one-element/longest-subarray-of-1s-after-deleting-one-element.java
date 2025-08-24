8aa99e6fce5d05c17a1c3c1d2e5e0df02206f488
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-08-24 12:33:41 +0530

    Time: 3 ms (77.92%) | Memory: 54.9 MB (99.80%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -31,36 +31,36 @@
 // }
 
 
-class Solution {
-    public int longestSubarray(int[] nums) {
-        int len = nums.length;
-        int ans = 0;
-        int oneIndex = -1;
-        int i = 0, j = 0;
+// class Solution {
+//     public int longestSubarray(int[] nums) {
+//         int len = nums.length;
+//         int ans = 0;
+//         int oneIndex = -1;
+//         int i = 0, j = 0;
 
-        while(j < len && i < len)
-        {
-            if(nums[j] == 1)
-                j++;
-            else if(nums[j] == 0 && oneIndex != -1)
-            {
-                i = oneIndex + 1;
-                oneIndex = j;
-                j++;
-            }
-            else if(nums[j] == 0 && oneIndex == -1)
-            {
-                oneIndex = j;
-                j++;
-            }
+//         while(j < len && i < len)
+//         {
+//             if(nums[j] == 1)
+//                 j++;
+//             else if(nums[j] == 0 && oneIndex != -1)
+//             {
+//                 i = oneIndex + 1;
+//                 oneIndex = j;
+//                 j++;
+//             }
+//             else if(nums[j] == 0 && oneIndex == -1)
+//             {
+//                 oneIndex = j;
+//                 j++;
+//             }
 
-            ans = Math.max(ans, (j - i - 1));
-        }
-        if(ans == len)
-            return --ans;
-        return ans;
-    }
-}
+//             ans = Math.max(ans, (j - i - 1));
+//         }
+//         if(ans == len)
+//             return --ans;
+//         return ans;
+//     }
+// }
 
 
 // class Solution {
@@ -89,3 +89,35 @@ class Solution {
 //         return maxLen;
 //     }
 // }
+
+
+class Solution {
+    public int longestSubarray(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+
+        int ct = 0;
+
+        for(int num : nums)
+        {
+            if(num == 1)
+                ct++;
+            else
+            {
+                list.add(ct);
+                ct = 0;
+            }
+        }
+        list.add(ct);
+        int len = list.size();
+        if(len == 1)
+            return nums.length - 1;
+
+        int ans = 0;
+        for(int i = 1; i < len; i++)
+        {
+            ans = Math.max(ans, list.get(i) + list.get(i - 1));
+        }
+
+        return ans;
+    }
+}
