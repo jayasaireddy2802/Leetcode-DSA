18f102485457854f164924bcc069fb9d250020cf
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-09-05 22:08:14 +0530

    Time: 1 ms (38.46%) | Memory: 40.4 MB (95.38%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.java
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.java
@@ -1,18 +1,35 @@
+// class Solution {
+
+//     public int getCountOfSetBits(long val)
+//     {
+//         int ct = 0;
+//         while(val > 0)
+//         {
+//             if((val & 1) != 0)
+//                 ct++;
+//             val = val >> 1;
+//         }
+
+//         return ct;
+//     }
+
+//     public int makeTheIntegerZero(int num1, int num2) {
+//         int k = 0;
+
+//         while(true)
+//         {
+//             long val = (long)num1 - (long)k*num2;
+//             if(val < 0)
+//                 return -1;
+//             int min_bits = getCountOfSetBits(val);
+//             if(min_bits <= k && k <= val)
+//                 return k;
+//             k++;
+//         }
+//     }
+// }
+
 class Solution {
-
-    public int getCountOfSetBits(long val)
-    {
-        int ct = 0;
-        while(val > 0)
-        {
-            if((val & 1) != 0)
-                ct++;
-            val = val >> 1;
-        }
-
-        return ct;
-    }
-
     public int makeTheIntegerZero(int num1, int num2) {
         int k = 0;
 
@@ -21,7 +38,7 @@ class Solution {
             long val = (long)num1 - (long)k*num2;
             if(val < 0)
                 return -1;
-            int min_bits = getCountOfSetBits(val);
+            int min_bits = Long.bitCount(val);
             if(min_bits <= k && k <= val)
                 return k;
             k++;
