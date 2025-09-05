f987861caaaa5a140f3e67c8f09b33dbf00cccee
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-09-05 22:07:33 +0530

    Time: 1 ms (38.46%) | Memory: 40.9 MB (44.62%) - LeetSync

---
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public int getCountOfSetBits(long val)
+    {
+        int ct = 0;
+        while(val > 0)
+        {
+            if((val & 1) != 0)
+                ct++;
+            val = val >> 1;
+        }
+
+        return ct;
+    }
+
+    public int makeTheIntegerZero(int num1, int num2) {
+        int k = 0;
+
+        while(true)
+        {
+            long val = (long)num1 - (long)k*num2;
+            if(val < 0)
+                return -1;
+            int min_bits = getCountOfSetBits(val);
+            if(min_bits <= k && k <= val)
+                return k;
+            k++;
+        }
+    }
+}
