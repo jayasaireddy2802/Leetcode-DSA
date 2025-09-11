8885972d6d6c40aaeb424c8a3e278f9125ff97c9
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-09-11 10:57:08 +0530

    Time: 37 ms (78.78%) | Memory: 45.6 MB (96.78%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -1,15 +1,53 @@
+// class Solution {
+
+//     private boolean isVowel(char ch)
+//     {
+//         ch = Character.toLowerCase(ch);
+//         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+//             return true;
+//         return false;
+//     }
+
+//     public String sortVowels(String s) {
+//         List<Character> list = new ArrayList<>();
+//         char[] arr = s.toCharArray();
+//         int len = arr.length;
+
+//         for(char ch : arr)
+//         {
+//             if(isVowel(ch))
+//             {
+//                 list.add(ch);
+//             }
+//         }
+
+//         Collections.sort(list);
+//         int j = 0;
+
+//         for(int i = 0; i < len; i++)
+//         {
+//             if(isVowel(arr[i]))
+//             {
+//                 arr[i] = list.get(j);
+//                 j++;
+//             }
+//         }
+
+//         return new String(arr);
+//     }
+// }
+
+
 class Solution {
 
     private boolean isVowel(char ch)
     {
         ch = Character.toLowerCase(ch);
-        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-            return true;
-        return false;
+        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');            
     }
 
     public String sortVowels(String s) {
-        List<Character> list = new ArrayList<>();
+        Map<Character, Integer> map = new HashMap<>();
         char[] arr = s.toCharArray();
         int len = arr.length;
 
@@ -17,19 +55,28 @@ class Solution {
         {
             if(isVowel(ch))
             {
-                list.add(ch);
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
             }
         }
 
-        Collections.sort(list);
-        int j = 0;
+        String sortedVowel = "AEIOUaeiou";
+        int l = sortedVowel.length();
+        int idx = 0;
 
         for(int i = 0; i < len; i++)
         {
             if(isVowel(arr[i]))
             {
-                arr[i] = list.get(j);
-                j++;
+                char ch;
+                while(idx < l && map.getOrDefault(sortedVowel.charAt(idx), 0) == 0)
+                {
+                    idx++;
+                }
+                if(idx < l){
+                    ch = sortedVowel.charAt(idx); 
+                    arr[i] = ch;
+                    map.put(ch, map.get(ch) - 1);
+                }
             }
         }
 
