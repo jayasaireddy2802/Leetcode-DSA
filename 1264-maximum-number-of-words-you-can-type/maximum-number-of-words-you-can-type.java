ffa1bfe1e4533ffebb4e48a371cd91363cc9dfb0
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-09-15 10:29:39 +0530

    Time: 2 ms (91.65%) | Memory: 42 MB (93.74%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
@@ -1,15 +1,44 @@
+// class Solution {
+//     public int canBeTypedWords(String text, String brokenLetters) {
+//         String[] list = text.split(" ");
+//         char[] arr = brokenLetters.toCharArray();
+//         int ct = 0;
+
+//         for(String str : list)
+//         {
+//             boolean canType = true;
+//             for(char ch : arr)
+//             {
+//                 if(str.indexOf(ch) != -1)
+//                 {
+//                     canType = false;
+//                     break;
+//                 }
+//             }
+//             if(canType)
+//                 ct++;
+//         }
+
+//         return ct;
+//     }
+// }
+
 class Solution {
     public int canBeTypedWords(String text, String brokenLetters) {
         String[] list = text.split(" ");
-        char[] arr = brokenLetters.toCharArray();
+        Set<Character> set = new HashSet<>();
+
+        for(char ch : brokenLetters.toCharArray())
+            set.add(ch);
+         
         int ct = 0;
 
         for(String str : list)
         {
             boolean canType = true;
-            for(char ch : arr)
+            for(char ch : str.toCharArray())
             {
-                if(str.indexOf(ch) != -1)
+                if(set.contains(ch))
                 {
                     canType = false;
                     break;
