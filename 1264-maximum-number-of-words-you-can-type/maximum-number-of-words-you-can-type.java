d7852e7242e436fd1b761f7cbca8ff672da5c94b
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-09-15 10:34:47 +0530

    Time: 2 ms (91.65%) | Memory: 42.5 MB (45.48%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
@@ -53,31 +53,67 @@
 // }
 
 
+// class Solution {
+//     public int canBeTypedWords(String text, String brokenLetters) {
+//         String[] list = text.split(" ");
+//         boolean[] broken = new boolean[28];
+
+//         for(char ch : brokenLetters.toCharArray())
+//         {
+//             broken[ch - 'a'] = true;
+//         }
+         
+//         int ct = 0;
+
+//         for(String str : list)
+//         {
+//             boolean canType = true;
+//             for(char ch : str.toCharArray())
+//             {
+//                 if(broken[ch - 'a'])
+//                 {
+//                     canType = false;
+//                     break;
+//                 }
+//             }
+//             if(canType)
+//                 ct++;
+//         }
+
+//         return ct;
+//     }
+// }
+
+
 class Solution {
     public int canBeTypedWords(String text, String brokenLetters) {
         String[] list = text.split(" ");
-        boolean[] broken = new boolean[28];
+        boolean[] broken = new boolean[26]; // 26 is enough for lowercase a-z
 
-        for(char ch : brokenLetters.toCharArray())
-        {
+        // Mark broken letters
+        for (int i = 0; i < brokenLetters.length(); i++) {
+            char ch = brokenLetters.charAt(i);
             broken[ch - 'a'] = true;
         }
-         
+
         int ct = 0;
 
-        for(String str : list)
-        {
+        // Check each word
+        for (int i = 0; i < list.length; i++) {
+            String str = list[i];
             boolean canType = true;
-            for(char ch : str.toCharArray())
-            {
-                if(broken[ch - 'a'])
-                {
+
+            for (int j = 0; j < str.length(); j++) {
+                char ch = str.charAt(j);
+                if (broken[ch - 'a']) {
                     canType = false;
                     break;
                 }
             }
-            if(canType)
+
+            if (canType) {
                 ct++;
+            }
         }
 
         return ct;
