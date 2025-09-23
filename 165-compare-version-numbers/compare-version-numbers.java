16bea08efc5e482d0224a31e7f86cfcc49c5cf40
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-09-23 11:35:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (93.05%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -1,40 +1,40 @@
-// class Solution {
-//     public int compareVersion(String version1, String version2) {
-//         int len1 = version1.length();
-//         int len2 = version2.length();
-//         int i = 0, j = 0;
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        int len1 = version1.length();
+        int len2 = version2.length();
+        int i = 0, j = 0;
     
-//         while(i < len1 || j < len2)
-//         {
-//             int num1 = 0;
-//             while(i < len1 && version1.charAt(i) != '.')
-//             {
-//                 num1 = num1*10 + (version1.charAt(i) - '0');
-//                 i++;
-//             }
+        while(i < len1 || j < len2)
+        {
+            int num1 = 0;
+            while(i < len1 && version1.charAt(i) != '.')
+            {
+                num1 = num1*10 + (version1.charAt(i) - '0');
+                i++;
+            }
 
-//             int num2 = 0;
-//             while(j < len2 && version2.charAt(j) != '.')
-//             {
-//                 num2 = num2*10 + (version2.charAt(j) - '0');
-//                 j++;
-//             }
+            int num2 = 0;
+            while(j < len2 && version2.charAt(j) != '.')
+            {
+                num2 = num2*10 + (version2.charAt(j) - '0');
+                j++;
+            }
 
-//             if(num1 > num2)
-//                 return 1;
+            if(num1 > num2)
+                return 1;
 
-//             if(num1 < num2)
-//                 return -1;
+            if(num1 < num2)
+                return -1;
 
-//             i++;
-//             j++;            
-//         }
+            i++;
+            j++;            
+        }
 
         
 
-//         return 0;
-//     }
-// }
+        return 0;
+    }
+}
 
 
 
@@ -66,36 +66,36 @@
 
 
 
-class Solution {
-    public int compareVersion(String version1, String version2) {
-        String[] st1 = version1.split("\\.");
-        String[] st2 = version2.split("\\.");   
-        int len1 = st1.length;
-        int len2 = st2.length; 
-        int i = 0;
-        int j = 0;
+// class Solution {
+//     public int compareVersion(String version1, String version2) {
+//         String[] st1 = version1.split("\\.");
+//         String[] st2 = version2.split("\\.");   
+//         int len1 = st1.length;
+//         int len2 = st2.length; 
+//         int i = 0;
+//         int j = 0;
 
-        while(i < len1 || j < len2)
-        {
-            int num1 = 0;
-            if(i < len1)
-            {
-                num1 = Integer.parseInt(st1[i++]);
-            }
+//         while(i < len1 || j < len2)
+//         {
+//             int num1 = 0;
+//             if(i < len1)
+//             {
+//                 num1 = Integer.parseInt(st1[i++]);
+//             }
             
-            int num2 = 0;
-            if(j < len2)
-                num2 = Integer.parseInt(st2[j++]);
+//             int num2 = 0;
+//             if(j < len2)
+//                 num2 = Integer.parseInt(st2[j++]);
             
-            if(num1 > num2)
-                return 1;
-            if(num2 > num1)
-                return -1;
-        }   
+//             if(num1 > num2)
+//                 return 1;
+//             if(num2 > num1)
+//                 return -1;
+//         }   
 
-        return 0;
-    }
-}
+//         return 0;
+//     }
+// }
 
 
 
