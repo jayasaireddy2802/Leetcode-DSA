66e08710521a5bc0cc88db3ddd561206d8fa4b07
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-10-07 11:44:53 +0530

    Time: 70 ms (33.79%) | Memory: 62.5 MB (33.33%) - LeetSync

--- 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.java
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.java
@@ -1,10 +1,11 @@
+
+
 // class Solution {
 //     public int[] avoidFlood(int[] rains) {
 //         int len = rains.length;
-//         boolean[] isWater = new boolean[10000000];
+//         Map<Integer, Integer> map = new HashMap<>();
 //         int[] ans = new int[len];
 //         List<Integer> list = new ArrayList<>();
-//         int todry = 0;
 
 //         for(int i = 0; i < len; i++)
 //         {
@@ -12,37 +13,44 @@
 //             if(lake == 0)
 //             {
 //                 list.add(i);
-
+//                 ans[i] = 1;
 //             }            
-//             else if(!isWater[lake])
+//             else
 //             {
-//                 isWater[lake] = true;
-//                 ans[i] = -1;
-//                 todry++;
-//             }
-//             else 
-//             {
-//                 if(list.isEmpty())
-//                     return new int[0];
-//                 else
+//                 if(map.containsKey(lake))
 //                 {
-//                     int idx = list.remove(0);
-//                     if(idx < lake)
+//                     int idx = -1;
+//                     if(!list.isEmpty())
+//                     {
+//                         int val = map.get(lake);
+//                         for(int l : list)
+//                         {
+//                             if(l > val)
+//                             {
+//                                 idx = l;
+//                                 break;
+//                             }
+//                         }
+//                         list.remove(Integer.valueOf(idx));
+
+//                     }
+//                     else
+//                         return new int[0];
+//                     if(idx == -1)
 //                         return new int[0];
 //                     ans[idx] = lake;
-//                     ans[i] = -1;
-                    
 //                 }
+//                 map.put(lake, i);
+//                 ans[i] = -1;
 //             }
+            
 //         }
         
-//         for(int i = 0; i < len; i++)
-//         {
-//             if(ans[i] == 0) ans[i] = 1;
-//         }
 //         return ans;
 //     }
 // }
+
+
 
 
 class Solution {
@@ -50,37 +58,27 @@ class Solution {
         int len = rains.length;
         Map<Integer, Integer> map = new HashMap<>();
         int[] ans = new int[len];
-        List<Integer> list = new ArrayList<>();
+        TreeSet<Integer> set = new TreeSet<>();
 
         for(int i = 0; i < len; i++)
         {
             int lake = rains[i];
             if(lake == 0)
             {
-                list.add(i);
+                set.add(i);
                 ans[i] = 1;
             }            
             else
             {
                 if(map.containsKey(lake))
                 {
-                    int idx = -1;
-                    if(!list.isEmpty())
-                    {
-                        int val = map.get(lake);
-                        for(int l : list)
-                        {
-                            if(l > val)
-                            {
-                                idx = l;
-                                break;
-                            }
-                        }
-                        list.remove(Integer.valueOf(idx));
-
+                    Integer idx = -1;
+                    if (map.containsKey(lake)) {
+                     idx = set.higher(map.get(lake)); 
+                    if (idx == null) return new int[0];      
+                    ans[idx] = lake; 
+                    set.remove(idx);
                     }
-                    else
-                        return new int[0];
                     if(idx == -1)
                         return new int[0];
                     ans[idx] = lake;
