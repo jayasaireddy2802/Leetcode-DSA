d83e3e145e8d6898e54309d8c5ebff961e934488
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-10-10 14:14:15 +0530

    Time: 2 ms (97.93%) | Memory: 59.6 MB (84.14%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
@@ -1,40 +1,56 @@
-// class Solution {    
-
-//     public int maximumEnergy(int[] energy, int k) {
-
-//         int n = energy.length;
-//         int ans = Integer.MIN_VALUE;
-
-//         for(int i = 0; i < n; i++)
-//         {
-//             int val = 0;
-//             for(int j = i; j < n; j = j + k)
-//             {
-//                 val += energy[j];
-//             }
-//             ans = Math.max(ans, val);
-//         }
-        
-//         return ans;
-//     }
-// }
-
 class Solution {    
 
     public int maximumEnergy(int[] energy, int k) {
 
         int n = energy.length;
         int ans = Integer.MIN_VALUE;
-        int[] dp = new int[n];
 
-        for(int i = n-1; i >= 0; i--)
+        for(int i = n - k; i < n; i++)
         {
-            dp[i] = energy[i];
-            if((i + k) < n)
-                dp[i] = dp[i] + dp[i + k];
-            ans = Math.max(dp[i], ans);
+            int val = 0;
+            for(int j = i; j >= 0; j = j - k)
+            {
+                val += energy[j];
+                ans = Math.max(ans, val);
+            }
         }
         
         return ans;
     }
 }
+
+// class Solution {    
+
+//     public int maximumEnergy(int[] energy, int k) {
+
+//         int n = energy.length;
+//         int ans = Integer.MIN_VALUE;
+//         int[] dp = new int[n];
+
+//         for(int i = n-1; i >= 0; i--)
+//         {
+//             dp[i] = energy[i];
+//             if((i + k) < n)
+//                 dp[i] = dp[i] + dp[i + k];
+//             ans = Math.max(dp[i], ans);
+//         }
+        
+//         return ans;
+//     }
+// }
+
+// class Solution {
+
+//     public int maximumEnergy(int[] energy, int k) {
+//         int n = energy.length;
+//         int ans = Integer.MIN_VALUE;
+//         for (int i = n - k; i < n; i++) {
+//             int sum = 0;
+//             for (int j = i; j >= 0; j -= k) {
+//                 sum += energy[j];
+//                 ans = Math.max(ans, sum);
+//             }
+//         }
+//         return ans;
+//     }
+// }
