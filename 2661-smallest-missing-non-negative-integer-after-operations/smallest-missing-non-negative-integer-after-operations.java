cbf2ca034f0cf3f5cf66a8531c22fce399ca8f77
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-10-16 16:10:14 +0530

    Time: 6 ms (84.77%) | Memory: 58.3 MB (58.38%) - LeetSync

--- 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.java
+++ 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.java
@@ -1,20 +1,46 @@
+// class Solution {
+//     public int findSmallestInteger(int[] nums, int value) {
+//         Map<Integer, Integer> map = new HashMap<>();
+
+//         for(int num : nums)
+//         {
+//             int mod = (num % value + value) % value;
+//             map.put(mod, map.getOrDefault(mod, 0) + 1);
+//         }
+            
+//         int i = 0;
+//         while(true)
+//         {
+//             int val = i % value;
+//             if(map.getOrDefault(val, 0) == 0)
+//                 return i;
+//             map.put(val, map.get(val) - 1);
+//             i++;
+//         }
+
+        
+//     }
+
+// }
+
+
 class Solution {
     public int findSmallestInteger(int[] nums, int value) {
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] map = new int[value];
 
         for(int num : nums)
         {
             int mod = (num % value + value) % value;
-            map.put(mod, map.getOrDefault(mod, 0) + 1);
+            map[mod]++;
         }
             
         int i = 0;
         while(true)
         {
             int val = i % value;
-            if(map.getOrDefault(val, 0) == 0)
+            if(map[val] == 0)
                 return i;
-            map.put(val, map.get(val) - 1);
+            map[val]--;
             i++;
         }
 
