8e90b02777ed60f7899ba0299bc963f4c7cff558
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-10-23 12:02:38 +0530

    Time: 1 ms (100.00%) | Memory: 42.2 MB (95.34%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.java
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.java
@@ -1,21 +1,41 @@
+// class Solution {
+//     public boolean hasSameDigits(String s) {
+//         int len = s.length();
+//         StringBuilder ans = new StringBuilder(s);
+
+//         while(len > 2)
+//         {
+//             StringBuilder str = new StringBuilder();
+//             int i = 0;
+//             while(i < len - 1)
+//             {
+//                 char val = (char)(((ans.charAt(i) - '0') + (ans.charAt(i + 1) - '0')) % 10 + '0');
+//                 str.append(val);
+//                 i++;
+//             }
+//             ans = str;
+//             len = str.length();
+//         }
+//         return (ans.charAt(0) == ans.charAt(1));
+//     }
+// }
+
+
 class Solution {
     public boolean hasSameDigits(String s) {
         int len = s.length();
-        StringBuilder ans = new StringBuilder(s);
+        int[] arr = new int[len];
 
-        while(len > 2)
+        for(int i = 0; i < len; i++)
+            arr[i] = s.charAt(i) - '0';
+
+        for(int i = len; i > 2; i--)
         {
-            StringBuilder str = new StringBuilder();
-            int i = 0;
-            while(i < len - 1)
+            for(int j = 0; j < i - 1; j++)
             {
-                char val = (char)(((ans.charAt(i) - '0') + (ans.charAt(i + 1) - '0')) % 10 + '0');
-                str.append(val);
-                i++;
+                arr[j] = (arr[j] + arr[j+1]) % 10;
             }
-            ans = str;
-            len = str.length();
         }
-        return (ans.charAt(0) == ans.charAt(1));
+        return (arr[0] == arr[1]);
     }
 }
