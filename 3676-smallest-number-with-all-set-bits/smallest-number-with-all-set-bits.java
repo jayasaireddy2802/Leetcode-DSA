63ca41986df95f0e3ea51fee5b6fb6531d5f2408
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-10-29 11:57:16 +0530

    Time: 1 ms (33.25%) | Memory: 41.1 MB (16.75%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
@@ -1,16 +1,38 @@
+// class Solution {
+
+//     public boolean isValid(int num)
+//     {
+//         while(num > 0)
+//         {
+//             if((num & 1) == 0)
+//                 return false;
+//             num = num >> 1;
+//         }
+            
+//         return true;
+        
+//     }
+//     public int smallestNumber(int n) {
+//         int ans = n;
+
+//         while(ans < 1000_000_000)
+//         {
+//             if(isValid(ans))
+//             {
+//                 return ans;
+//             }
+//             ans++;
+//         }
+//         return -1;
+//     }
+// }
+
+
 class Solution {
 
     public boolean isValid(int num)
     {
-        while(num > 0)
-        {
-            if((num & 1) == 0)
-                return false;
-            num = num >> 1;
-        }
-            
-        return true;
-        
+        return (Integer.bitCount(num) == (32 - Integer.numberOfLeadingZeros(num)));        
     }
     public int smallestNumber(int n) {
         int ans = n;
