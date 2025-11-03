ead8014f9fc22a81b98ca359e51b9b923f7d230b
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-11-03 10:35:54 +0530

    Time: 8 ms (82.68%) | Memory: 92.8 MB (5.68%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
@@ -1,49 +1,81 @@
+// class Solution {
+//     public int minCost(String colors, int[] neededTime) {
+//         int len = neededTime.length;
+//         int ans = 0;
+//         char prev = colors.charAt(0);
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
+//         pq.add(neededTime[0]);
+
+//         for(int i = 1; i < len; i++)
+//         {
+//             if(colors.charAt(i) == prev)
+//             {
+//                 pq.add(neededTime[i]);
+//             }
+//             else
+//             {
+//                 int val = 0;
+//                 int res = 0;
+
+//                 while(!pq.isEmpty())
+//                 {
+//                     val = pq.poll();
+//                     res = res + val;
+//                 }
+
+//                 res = res - val;
+//                 ans = ans + res;
+
+//                 pq.clear();
+//                 pq.add(neededTime[i]);
+//             }
+
+//             prev = colors.charAt(i);
+//         }
+
+//         int val = 0;
+//         int res = 0;
+//         while(!pq.isEmpty())
+//         {
+//             val = pq.poll();
+//             res = res + val;
+//         }
+
+//         res = res - val;
+//         ans = ans + res;
+
+//         return ans;
+//     }
+// }
+
+
 class Solution {
     public int minCost(String colors, int[] neededTime) {
         int len = neededTime.length;
-        int ans = 0;
+        int total = 0;
+        int maxTime = neededTime[0];
+        int sum = neededTime[0];
         char prev = colors.charAt(0);
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        pq.add(neededTime[0]);
 
         for(int i = 1; i < len; i++)
         {
-            if(colors.charAt(i) == prev)
+            if(prev == colors.charAt(i))
             {
-                pq.add(neededTime[i]);
+                sum = sum + neededTime[i];
+                maxTime = Math.max(maxTime, neededTime[i]);
             }
             else
             {
-                int val = 0;
-                int res = 0;
-
-                while(!pq.isEmpty())
-                {
-                    val = pq.poll();
-                    res = res + val;
-                }
-
-                res = res - val;
-                ans = ans + res;
-
-                pq.clear();
-                pq.add(neededTime[i]);
+                total = total + (sum - maxTime);
+                maxTime = neededTime[i];
+                sum = neededTime[i];
             }
 
             prev = colors.charAt(i);
         }
 
-        int val = 0;
-        int res = 0;
-        while(!pq.isEmpty())
-        {
-            val = pq.poll();
-            res = res + val;
-        }
+        total = total + (sum - maxTime);
 
-        res = res - val;
-        ans = ans + res;
-
-        return ans;
+        return total;
     }
 }
