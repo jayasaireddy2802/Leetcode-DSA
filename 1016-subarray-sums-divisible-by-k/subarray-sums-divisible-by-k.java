5b350471ba54426ab743364b4187173253acec86
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-11-27 20:36:55 +0530

    Time: 26 ms (23.84%) | Memory: 51.6 MB (16.46%) - LeetSync

---
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -0,0 +1,74 @@
+// class Solution {
+//     public int subarraysDivByK(int[] nums, int k) {
+//         int len = nums.length;
+//         int ct = 0;
+
+//         for(int i = 0; i < len; i++)
+//         {
+//             for(int j = i; j < len; j++)
+//             {
+//                 int sum = 0;
+//                 for(int m = i; m <= j; m++)
+//                 {
+//                     sum = sum + nums[m];
+//                 }
+//                 if(sum % k == 0)
+//                     ct++;
+//             }
+//         }
+
+//         return ct;
+//     }
+// }
+
+
+// class Solution {
+//     public int subarraysDivByK(int[] nums, int k) {
+//         int len = nums.length;
+//         int ct = 0;
+
+
+//         long[] prefix = new long[len + 1];
+//         for(int i = 1; i <= len; i++)
+//         {
+//             prefix[i] = prefix[i-1] + nums[i - 1];
+//         }
+        
+//         for(int i = 0; i < len; i++)
+//         {
+//             for(int j = i; j < len; j++)
+//             {
+//                 long sum = prefix[j + 1] - prefix[i];
+//                 if(sum % k == 0)
+//                     ct++;
+//             }
+//         }
+
+//         return ct;
+//     }
+// }
+
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int len = nums.length;
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int ans = 0;
+
+        map.put(sum, 1);
+
+        for(int i = 0; i < len; i++)
+        {
+            sum += nums[i];
+            int rem = (sum % k + k) % k;
+            if(map.containsKey(rem))
+                ans += map.get(rem);;
+            map.put(rem, map.getOrDefault(rem, 0) + 1);
+        }
+
+        return ans;
+
+
+    }
+}
+
