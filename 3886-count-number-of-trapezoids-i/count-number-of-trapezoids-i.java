7e9770c1b882bb3b30c3be6f47785d2a98a49f1c
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-12-03 13:43:48 +0530

    Time: 40 ms (65.67%) | Memory: 188.4 MB (33.33%) - LeetSync

--- 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.java
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.java
@@ -1,26 +1,55 @@
+// class Solution {
+//     public int countTrapezoids(int[][] points) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         long ans = 0;
+//         int mod = 1000000007;
+
+//         for(int[] point : points)
+//         {
+//             int y = point[1];
+//             map.put(y, map.getOrDefault(y, 0) + 1);
+//         }
+
+//         int prevEdges = 0;
+
+//         for(int key : map.keySet())
+//         {
+//             long curr = map.get(key);
+//             long edges = (curr*(curr - 1))/2;
+//             ans = (ans + (edges*prevEdges)) % mod;
+//             prevEdges += edges;
+
+//         }
+
+//         return(int) ans;
+//     }
+// }
+
+
 class Solution {
     public int countTrapezoids(int[][] points) {
         Map<Integer, Integer> map = new HashMap<>();
-        long ans = 0;
         int mod = 1000000007;
 
         for(int[] point : points)
         {
+            int x = point[0];
             int y = point[1];
+
             map.put(y, map.getOrDefault(y, 0) + 1);
         }
 
-        int prevEdges = 0;
+        long ans = 0;
+        int totalLines = 0;
 
         for(int key : map.keySet())
         {
-            long curr = map.get(key);
-            long edges = (curr*(curr - 1))/2;
-            ans = (ans + (edges*prevEdges)) % mod;
-            prevEdges += edges;
-
+            long value = map.get(key);
+            long lines = (value*(value - 1) / 2) % mod;
+            ans = (ans + (lines * totalLines) )% mod;
+            totalLines += lines;
         }
 
-        return(int) ans;
+        return (int)ans;
     }
 }
