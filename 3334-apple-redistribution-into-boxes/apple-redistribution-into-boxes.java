14af6f63f7d79d702096a3a54202f29258b6c61e
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-12-24 10:30:30 +0530

    Time: 5 ms (26.41%) | Memory: 44.2 MB (79.53%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
@@ -1,27 +1,50 @@
+// class Solution {
+//     public int minimumBoxes(int[] apple, int[] capacity) {
+//         Arrays.sort(capacity);
+//         for(int i = 0, j = capacity.length - 1; i < j; i++, j--)
+//         {
+//             int temp = capacity[i];
+//             capacity[i] = capacity[j];
+//             capacity[j] = temp; 
+//         }
+//         int len = capacity.length;
+//         int sum = 0;
+
+//         for(int val : apple)
+//             sum += val;
+
+//         int i = 0;
+//         while(i < len)
+//         {
+//             if(sum <= 0)
+//                 break;
+//             sum -= capacity[i];
+//             i++;
+//         }
+
+//         return i;
+//     }
+// }
+
+
 class Solution {
     public int minimumBoxes(int[] apple, int[] capacity) {
         Arrays.sort(capacity);
-        for(int i = 0, j = capacity.length - 1; i < j; i++, j--)
-        {
-            int temp = capacity[i];
-            capacity[i] = capacity[j];
-            capacity[j] = temp; 
-        }
         int len = capacity.length;
         int sum = 0;
 
         for(int val : apple)
             sum += val;
 
-        int i = 0;
-        while(i < len)
+        int i = capacity.length - 1;
+        while(i >= 0)
         {
             if(sum <= 0)
                 break;
             sum -= capacity[i];
-            i++;
+            i--;
         }
 
-        return i;
+        return capacity.length - i - 1;
     }
 }
