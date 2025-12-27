61e7be83202038f15ba31dc860f3730f8b935649
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-12-27 21:59:49 +0530

    Time: 51 ms (95.69%) | Memory: 125.6 MB (88.88%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.java
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.java
@@ -1,53 +1,50 @@
 class Solution {
     public int mostBooked(int n, int[][] meetings) {
-        // Sort the meetings based on their start times
-        Arrays.sort(meetings, (m1, m2) -> Integer.compare(m1[0], m2[0]));
 
-        // Array to track how many times each room is used
-        int[] usageCount = new int[n];
-        // Array to store when each room becomes available
-        long[] roomAvailability = new long[n];
+        
+        Arrays.sort(meetings, (a, b) -> a[0] - b[0]);
 
-        for (int[] meeting : meetings) {
-            int startTime = meeting[0];
-            int endTime = meeting[1];
-            boolean isAssigned = false;
+        int[] roomUsed = new int[n];     
+        long[] freeAt = new long[n];      
 
-            int selectedRoom = -1;
-            long earliestFreeTime = Long.MAX_VALUE;
+        for (int[] meet : meetings) {
+            int start = meet[0];
+            int end = meet[1];
+            int duration = end - start;
 
-            // Look for an available room at the meeting's start time
+            int chosenRoom = -1;
+            long minFreeTime = Long.MAX_VALUE;
+
             for (int i = 0; i < n; i++) {
-                if (roomAvailability[i] <= startTime) {
-                    roomAvailability[i] = endTime;
-                    usageCount[i]++;
-                    isAssigned = true;
+                if (freeAt[i] <= start) {
+                    chosenRoom = i;
                     break;
                 }
 
-                // Track the room that becomes available the earliest
-                if (roomAvailability[i] < earliestFreeTime) {
-                    earliestFreeTime = roomAvailability[i];
-                    selectedRoom = i;
+               
+                if (freeAt[i] < minFreeTime) {
+                    minFreeTime = freeAt[i];
+                    chosenRoom = i;
                 }
             }
 
-            // If no room was free, delay the meeting to the earliest available room
-            if (!isAssigned) {
-                long newEndTime = roomAvailability[selectedRoom] + (endTime - startTime);
-                roomAvailability[selectedRoom] = newEndTime;
-                usageCount[selectedRoom]++;
+            if (freeAt[chosenRoom] <= start) {
+                freeAt[chosenRoom] = end;
+            } else {
+                freeAt[chosenRoom] += duration;
             }
+
+            roomUsed[chosenRoom]++;
         }
 
-        // Find the room with the maximum usage
-        int mostUsedRoom = 0;
+        
+        int answer = 0;
         for (int i = 1; i < n; i++) {
-            if (usageCount[i] > usageCount[mostUsedRoom]) {
-                mostUsedRoom = i;
+            if (roomUsed[i] > roomUsed[answer]) {
+                answer = i;
             }
         }
 
-        return mostUsedRoom;
+        return answer;
     }
 }
