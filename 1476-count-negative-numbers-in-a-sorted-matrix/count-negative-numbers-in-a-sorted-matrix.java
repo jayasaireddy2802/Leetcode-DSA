3c0b5752f4eebae5ae276dfddcc264d40269f176
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-12-28 14:57:09 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (7.96%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
@@ -18,6 +18,46 @@
 // }
 
 
+// class Solution {
+//     public int binarySearch(int r, int[][] grid)
+//     {
+//         int cols = grid[0].length;
+//         int i = 0, j = cols - 1;
+//         int idx = cols;
+
+//         while(i <= j)
+//         {
+//             int mid = (i + j)/2;
+
+//             if(grid[r][mid] < 0)
+//             {
+//                 idx = mid;
+//                 j = mid - 1;
+//             }
+//             else
+//                 i = mid + 1;
+//         }
+
+//         return cols - idx;
+//     }
+
+//     public int countNegatives(int[][] grid) {
+//         int ans = 0;
+//         int rows = grid.length;
+        
+//         for(int i = 0; i < rows; i++)
+//         {
+//             int ct = binarySearch(i, grid);
+//             ans += ct;
+//         }
+
+//         return ans;
+//     }
+// }
+
+
+
+
 class Solution {
     public int binarySearch(int r, int[][] grid)
     {
@@ -44,11 +84,21 @@ class Solution {
     public int countNegatives(int[][] grid) {
         int ans = 0;
         int rows = grid.length;
-        
-        for(int i = 0; i < rows; i++)
+        int cols = grid[0].length;
+        int row = rows - 1;
+        int col = 0;
+
+        while(col < cols && row >= 0)
         {
-            int ct = binarySearch(i, grid);
-            ans += ct;
+            if(grid[row][col] >= 0)
+            {
+                col++;
+            }
+            else
+            {
+                ans += (cols - col);
+                row--;
+            }
         }
 
         return ans;
