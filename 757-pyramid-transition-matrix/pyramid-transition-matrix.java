2ad86d0f89738ed83a4cc83b9475ee7a6b092181
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2025-12-29 16:19:23 +0530

    Time: 1473 ms (24.21%) | Memory: 48.4 MB (36.31%) - LeetSync

---
+++ 757-pyramid-transition-matrix/pyramid-transition-matrix.java
@@ -0,0 +1,58 @@
+class Solution {
+
+    public void getNextRows(String curr, Map<String, List<Character>> map, int idx, StringBuilder sb, List<String> list)
+    {
+        if(idx == curr.length() - 1)
+        {
+            list.add(sb.toString());
+            return ;
+        }
+
+        String key = curr.substring(idx, idx + 2);
+
+        if(!map.containsKey(key))
+            return ;
+        
+        for(char ch : map.get(key))
+        {
+            sb.append(ch);
+            getNextRows(curr, map, idx + 1, sb, list);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+    }
+
+    public boolean solve(String curr, Map<String, List<Character>> map)
+    {
+        int len = curr.length();
+        if(len == 1)
+            return true;
+
+        List<String> nextrows = new ArrayList<>();
+        getNextRows(curr, map, 0, new StringBuilder(), nextrows);
+
+        for(String row : nextrows)
+        {
+            if(solve(row, map)) return true;
+        }
+
+        return false;
+    }
+
+    public boolean pyramidTransition(String bottom, List<String> allowed) {
+        
+        Map<String, List<Character>> map = new HashMap<>();
+
+        for(String str : allowed)
+        {
+            String substr = str.substring(0,2);
+            List<Character> list = map.getOrDefault(substr, null);
+            if(list == null)
+                list = new ArrayList<>();
+            list.add(str.charAt(2));
+            map.put(substr, list);
+        }
+
+        return solve(bottom, map);
+    }
+}
