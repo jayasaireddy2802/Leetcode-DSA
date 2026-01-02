0042a189ed68154062e717578710b6ed95bcd031
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2026-01-02 13:02:44 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (25.57%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -1,19 +1,38 @@
+// class Solution {
+//     public int repeatedNTimes(int[] nums) {
+//         int len = nums.length;
+//         int n = len / 2;
+
+//         Map<Integer, Integer> map = new HashMap<>();
+
+//         for(int num : nums)
+//             map.put(num, map.getOrDefault(num, 0) + 1);
+        
+//         for(int key : map.keySet())
+//         {
+//             if(map.get(key) == n)
+//                 return key;
+//         }
+
+//         return -1;        
+//     }
+// }
+
+
 class Solution {
     public int repeatedNTimes(int[] nums) {
         int len = nums.length;
         int n = len / 2;
 
-        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
 
         for(int num : nums)
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        
-        for(int key : map.keySet())
         {
-            if(map.get(key) == n)
-                return key;
-        }
+            if(set.contains(num))
+                return num;
+            set.add(num);
+        }    
 
-        return -1;        
+        return -1;
     }
 }
