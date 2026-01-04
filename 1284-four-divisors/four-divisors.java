9600ade7729bc937812506441dfe6e8bd2f80022
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2026-01-04 10:40:06 +0530

    Time: 1782 ms (7.07%) | Memory: 46.5 MB (33.08%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.java
@@ -0,0 +1,79 @@
+// class Solution {
+
+//     public int solve(int num )
+//     {
+//         List<Integer> list = new ArrayList<>();
+
+//         for(int i = 1; i <= num; i++)
+//         {
+//             if(num % i == 0)
+//                 list.add(i);
+//         }
+
+//         int sum = 0;
+//         if(list.size() == 4)
+//         {
+//             for(int val : list)
+//                 sum += val;
+//         }
+
+//         if(sum == 0)
+//             return -1;
+//         return sum;
+//     }
+//     public int sumFourDivisors(int[] nums) {
+//         int sum = 0;
+
+//         for(int num : nums)
+//         {
+//             int val = solve(num);
+//             if(val != -1)
+//             {
+//                 sum += val;
+//             }
+//         }
+
+//         return sum;
+        
+//     }
+// }
+
+
+class Solution {
+
+    public int solve(int num )
+    {
+        int ct = 0;
+        int sum = 0;
+
+        for(int i = 1; i <= num; i++)
+        {
+            if(ct >= 4)
+                return 0;
+            if(num % i == 0)
+            {
+                sum += i;
+                ct++;
+            }
+        }
+
+        if(ct == 4)
+            return sum;
+        return 0;
+    }
+    public int sumFourDivisors(int[] nums) {
+        int sum = 0;
+
+        for(int num : nums)
+        {
+            int val = solve(num);
+            if(val != -1)
+            {
+                sum += val;
+            }
+        }
+
+        return sum;
+        
+    }
+}
