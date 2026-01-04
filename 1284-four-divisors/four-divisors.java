446102e1085e448d240ec3e434d72adf7ce8e8b3
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2026-01-04 11:55:41 +0530

    Time: 10 ms (79.55%) | Memory: 46.2 MB (81.57%) - LeetSync

--- 1284-four-divisors/four-divisors.java
+++ 1284-four-divisors/four-divisors.java
@@ -39,21 +39,75 @@
 // }
 
 
+// class Solution {
+
+//     public int solve(int num )
+//     {
+//         int ct = 0;
+//         int sum = 0;
+
+//         for(int i = 1; i <= num; i++)
+//         {
+//             if(ct >= 4)
+//                 return 0;
+//             if(num % i == 0)
+//             {
+//                 sum += i;
+//                 ct++;
+//             }
+//         }
+
+//         if(ct == 4)
+//             return sum;
+//         return 0;
+//     }
+//     public int sumFourDivisors(int[] nums) {
+//         int sum = 0;
+
+//         for(int num : nums)
+//         {
+//             int val = solve(num);
+//             if(val != -1)
+//             {
+//                 sum += val;
+//             }
+//         }
+
+//         return sum;
+        
+//     }
+// }
+
+
+
+
 class Solution {
 
     public int solve(int num )
     {
         int ct = 0;
         int sum = 0;
+        int num1 = (int) Math.sqrt(num);
 
-        for(int i = 1; i <= num; i++)
-        {
-            if(ct >= 4)
-                return 0;
+        for(int i = 1; i <= num1; i++)
+        {            
+            if(ct > 4)
+                break;
             if(num % i == 0)
             {
-                sum += i;
-                ct++;
+                int j = num / i;
+
+                if(i == j)
+                {
+                    sum += i;
+                    ct++;
+                }
+                else
+                {
+                    sum += i;
+                    sum += (num / i);
+                    ct += 2;
+                }
             }
         }
 
