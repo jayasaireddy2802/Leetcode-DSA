77bd0ca7d9052fffc19af94ff7f0d320ec8b40c5
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2026-03-08 15:30:52 +0530

    Time: 3 ms (29.39%) | Memory: 43.7 MB (20.75%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,66 +1,31 @@
-
-// wrong approach
-// class Solution {
-//     public String findDifferentBinaryString(String[] nums) {
-//         int ans = 0;
-//         int len = nums[0].length();
-//         Set<String> set = new HashSet<>();
-//         for(String binary : nums)
-//         {
-//             int num = Integer.parseInt(binary, 2);
-//             ans = ans ^ num;
-//             set.add(binary);
-//         }
-//         String res = Integer.toBinaryString(~ans);
-//         String str = new StringBuilder(res).reverse().substring(0, len);
-//         str =  new StringBuilder(str).reverse().toString();
-//         if(set.contains(str))
-//             return res;
-//         else
-//             return str;
-
-//     }
-// }
-
-
-
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-     
+        Set<String> set = new HashSet<>();
+
+        for(String str : nums)
+        {
+            set.add(str);
+        }
+
+        int num = 0;
         int len = nums[0].length();
-        int size = (int)1e6;
-
-        int[] arr = new int[size];
-
-        for(String binary : nums)
+        int i = 0;
+        while(i < len)
         {
-            int num = Integer.parseInt(binary, 2);
-             arr[num] = 1;
+            num = num | (1 << i);
+            i++;
         }
-        for(int i = 0; i < size; i++)
+
+        for(int j = 0; j <= num; j++)
         {
-            if(arr[i] == 0)
-            {
-                String ans = Integer.toBinaryString(i);
-
-               if(i == 0)
-               {
-                int j = 0;
-                String res = "";
-                while(j < len){
-                    res = res + '0';
-                    j++;
-                }
-
-                return res;
-               }
-                if(ans.length() == len)
-                    return ans;
+            String binaryString = Integer.toString(j, 2);
+            while (binaryString.length() < len) {
+                binaryString = "0" + binaryString;
             }
+            if(!set.contains(binaryString))
+                return binaryString;
         }
+
         return "";
     }
 }
-
-
-
