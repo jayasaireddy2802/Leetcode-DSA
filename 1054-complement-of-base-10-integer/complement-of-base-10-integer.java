6464e647fa23a15e2862e7ddb2ea4302bcdd7e77
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2026-03-11 10:41:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (82.81%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -1,14 +1,33 @@
+// class Solution {
+//     public int bitwiseComplement(int n) {
+//         String binaryString = Integer.toString(n, 2);
+//         StringBuilder sb = new StringBuilder();
+//         int len = binaryString.length();
+
+//         for(int i = 0; i < len; i++)
+//         {
+//             sb.append(binaryString.charAt(i) == '0' ? '1' : '0');
+//         }
+
+//         return Integer.parseInt(sb.toString(), 2);
+//     }
+// }
+
+
 class Solution {
     public int bitwiseComplement(int n) {
         String binaryString = Integer.toString(n, 2);
-        StringBuilder sb = new StringBuilder();
-        int len = binaryString.length();
+        int len = binaryString.length() - 1;
+        int num = 0;
 
-        for(int i = 0; i < len; i++)
+        for(int i = len; i >= 0; i--)
         {
-            sb.append(binaryString.charAt(i) == '0' ? '1' : '0');
+            if(binaryString.charAt(i) == '0')
+            {
+                num = num | (1 << len - i);
+            }
         }
 
-        return Integer.parseInt(sb.toString(), 2);
+        return num;
     }
 }
