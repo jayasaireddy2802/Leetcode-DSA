184135692db22e194e8fa4bdfa500fcc5c75e5d5
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2026-03-14 09:46:14 +0530

    Time: 3 ms (60.56%) | Memory: 44 MB (55.22%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -30,36 +30,74 @@
 // }
 
 
-class Solution {
-    int ct;
-    public String solve(int idx, char prevChar, StringBuilder sb, int n, int k)
-    {
-        if(idx >= n)
-        {
-            if(k == ct)
-                return sb.toString();
-            ct++;
-            return "";
-        }
+// class Solution {
+//     int ct;
+//     public String solve(int idx, char prevChar, StringBuilder sb, int n, int k)
+//     {
+//         if(idx >= n)
+//         {
+//             if(k == ct)
+//                 return sb.toString();
+//             ct++;
+//             return "";
+//         }
         
-        for(char ch = 'a'; ch < 'd'; ch++)
-        {
-            if(ch == prevChar)
-                continue;
-            sb.append(ch);
+//         for(char ch = 'a'; ch < 'd'; ch++)
+//         {
+//             if(ch == prevChar)
+//                 continue;
+//             sb.append(ch);
 
-            String str = solve(idx + 1, ch, sb, n, k);
-            if(!str.equals(""))
-                return str;
-            sb.deleteCharAt(sb.length() -1 );
+//             String str = solve(idx + 1, ch, sb, n, k);
+//             if(!str.equals(""))
+//                 return str;
+//             sb.deleteCharAt(sb.length() -1 );
+//         }
+//         return "";
+//     }
+//     public String getHappyString(int n, int k) {
+//         StringBuilder sb = new StringBuilder();
+//         int idx = 0;
+//         ct = 0;
+//         String str = solve(idx, 'd', sb, n, k-1);
+//         return str;
+//     }
+// }
+
+
+class Solution {
+    List<String> list;
+    char[] arr = {'a', 'b', 'c'};
+
+    public void solve(StringBuilder sb, int n, int k)
+    {
+        if(list.size() >= k)
+            return;
+
+        if(sb.length() == n)
+        {
+            list.add(sb.toString());
+            return;
         }
-        return "";
+
+        for(int i = 0; i < 3; i++)
+        {
+            if(sb.length() == 0 || sb.charAt(sb.length()-1) != arr[i])
+            {
+                sb.append(arr[i]);
+                solve(sb, n, k);
+                sb.deleteCharAt(sb.length()-1);
+            }
+        }
     }
+
     public String getHappyString(int n, int k) {
-        StringBuilder sb = new StringBuilder();
-        int idx = 0;
-        ct = 0;
-        String str = solve(idx, 'd', sb, n, k-1);
-        return str;
+        list = new ArrayList<>();
+        solve(new StringBuilder(), n, k);
+
+        if(list.size() < k)
+            return "";
+
+        return list.get(k-1);
     }
 }
