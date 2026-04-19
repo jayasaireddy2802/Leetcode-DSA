de58bb5a43fd5574f40d230a2b26694ddc3ec5c7
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2026-04-19 12:57:51 +0530

    Time: 3 ms (98.64%) | Memory: 89.5 MB (97.29%) - LeetSync

--- 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.java
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.java
@@ -21,41 +21,70 @@
 // }
 
 
+// class Solution {
+
+//     public int binarySearch(int[] nums1, int num2, int idx)
+//     {
+//         int len1 = nums1.length;
+//         int i = 0;
+//         int j = Math.min(idx, len1 - 1);
+//         int res = -1;
+
+//         while(i <= j)
+//         {
+//             int mid = i + (j - i) / 2 ;
+//             if(nums1[mid] <= num2) 
+//             {
+//                 res = mid;
+//                 j = mid - 1;
+//             }
+//             else 
+//                 i = mid + 1;               
+//         }
+
+//         return res;
+//     }
+//     public int maxDistance(int[] nums1, int[] nums2) {
+//         int maxi = 0;
+//         int len1 = nums1.length;
+//         int len2 = nums2.length;
+
+//         for(int j = 0; j < len2; j++)
+//         {
+//             int i = binarySearch(nums1, nums2[j], j);
+//             if(i != -1)
+//                 maxi = Math.max(maxi, j - i);
+//         }
+
+//         return maxi;
+//     }
+// }
+
+
+
 class Solution {
 
-    public int binarySearch(int[] nums1, int num2, int idx)
+    public int maxDistance(int[] nums1, int[] nums2) 
     {
         int len1 = nums1.length;
+        int len2 = nums2.length;
         int i = 0;
-        int j = Math.min(idx, len1 - 1);
-        int res = -1;
+        int j = 0;
+        int res = 0;
 
-        while(i <= j)
+        while(i < len1 && j < len2)
         {
-            int mid = i + (j - i) / 2 ;
-            if(nums1[mid] <= num2) 
-            {
-                res = mid;
-                j = mid - 1;
-            }
+            if(nums1[i] > nums2[j]) i++;
             else 
-                i = mid + 1;               
+            {
+                res = Math.max(res, j - i);
+                j++;
+            }
+
         }
+
 
         return res;
-    }
-    public int maxDistance(int[] nums1, int[] nums2) {
-        int maxi = 0;
-        int len1 = nums1.length;
-        int len2 = nums2.length;
 
-        for(int j = 0; j < len2; j++)
-        {
-            int i = binarySearch(nums1, nums2[j], j);
-            if(i != -1)
-                maxi = Math.max(maxi, j - i);
-        }
-
-        return maxi;
     }
 }
