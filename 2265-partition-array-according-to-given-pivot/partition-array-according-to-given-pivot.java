96afba8e3b5c1466ebfcdf98978c975a60450a58
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2026-06-08 20:17:23 +0530

    Time: 5 ms (73.56%) | Memory: 160 MB (49.83%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,125 +1,34 @@
-// class Solution {
-//     public int[] pivotArray(int[] nums, int pivot) {
-//         List<Integer> list1 = new ArrayList<>();
-//         List<Integer> list2 = new ArrayList<>();
-//         List<Integer> list3 = new ArrayList<>();
-//         int n = nums.length;
-//         int[] ans = new int[n];
-
-//         for(int num : nums)
-//         {
-//             if(num == pivot)
-//                 list2.add(num);
-//             else if(num < pivot)
-//                 list1.add(num);
-//             else
-//                 list3.add(num);
-//         }
-
-//         int i = 0;
-//         for(int x : list1)
-//         {
-//             ans[i] = x;
-//             i++;
-//         }
-//         for(int x : list2)
-//         {
-//             ans[i] = x;
-//             i++;
-//         }
-//         for(int x : list3)
-//         {
-//             ans[i] = x;
-//             i++;
-//         }
-
-//         return ans;
-//     }
-// }
-
-
-
-// class Solution {
-//     public int[] pivotArray(int[] nums, int pivot) {
-//         int n = nums.length;
-//         int[] ans = new int[n];
-//         int ct = 0;
-//         int idx = 0;
-
-//         for(int i = 0; i < n; i++)
-//         {
-//             if(nums[i] == pivot)
-//             {
-//                 ct++;
-//             }
-//         }
-
-//         for(int num : nums)
-//         {
-//             if(num < pivot)
-//                 idx++;
-//         }
-
-//         while(ct > 0)
-//         {
-//             ans[idx] = pivot;
-//             idx++;
-//             ct--;
-//         }
-
-//         int i = 0;
-//         int j = idx;
-
-//         for(int num : nums)
-//         {
-//             if(num < pivot)
-//             {
-//                 ans[i] = num;
-//                 i++;
-//             }
-//             else if(num > pivot)
-//             {
-//                 ans[j] = num;
-//                 j++;
-//             }
-            
-//         }
-
-//         return ans;
-//     }
-// }
-
-
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int n = nums.length;
-        int[] ans = new int[n];
+        int len = nums.length;
+        int[] res = new int[len];
+        int ct = 0, pivotCount = 0;
+
+        for(int k = 0; k < len; k++)
+        {
+            if(nums[k] < pivot)
+                ct++;
+            if(nums[k] == pivot)
+                pivotCount++;
+        }
+
+        int j = ct;
+        while(pivotCount > 0)
+        {
+            res[j++] = pivot;
+            pivotCount--;
+        }
+
         int i = 0;
-        int j = n-1, k=0, l=n-1;
 
-        while(i < n && j >= 0)
+        for(int k = 0; k < len; k++)
         {
-            if(nums[i] < pivot){
-                ans[k] = nums[i];
-                k++;
-            }
-            if(nums[j] > pivot)
-            {
-                ans[l] = nums[j];
-                l--;
-            }
-            i++;
-            j--;
+            if(nums[k] < pivot)
+                res[i++] = nums[k];
+            else if(nums[k] > pivot)
+                res[j++] = nums[k];
         }
 
-       
-
-        while((l-k) >= 0)
-        {
-            ans[k] = pivot;
-            k++;
-        }
-
-        return ans;
+        return res;
     }
 }
