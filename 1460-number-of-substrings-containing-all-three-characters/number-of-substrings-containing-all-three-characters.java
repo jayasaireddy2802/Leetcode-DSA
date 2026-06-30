c3a2a760d9219b46c2b29d1e357836e58846dfd8
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2026-06-30 22:24:26 +0530

    Time: 10 ms (95.55%) | Memory: 46.3 MB (62.27%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,34 +1,66 @@
+// class Solution {
+//     public int numberOfSubstrings(String s) {
+//         int len = s.length();
+//         int count_A = 0;
+//         int count_B = 0;
+//         int count_C = 0;
+//         int ct = 0;
+
+//         for(int i = 0; i < len; i++)
+//         {
+//             count_A = 0;
+//             count_B = 0;
+//             count_C = 0;
+//             for(int j = i; j < len; j++)
+//             {
+//                 char ch = s.charAt(j);
+//                 if(ch == 'a') count_A++;
+//                 else if(ch == 'b') count_B++;
+//                 else if(ch == 'c') count_C++;
+
+//                 if(count_A > 0 && count_B > 0 && count_C > 0) ct++;
+//             }
+//         }
+
+//         return ct;
+//     }
+// }
+
+
+
 class Solution {
     public int numberOfSubstrings(String s) {
-
-        Map<Character, Integer> map = new HashMap<>();
-        int i = 0, j = 0;
         int len = s.length();
-        int ct = 0;
+        int count_A = 0;
+        int count_B = 0;
+        int count_C = 0;
+        int ct = 0, i = 0, j = 0;
 
         while(j < len)
         {
             char ch = s.charAt(j);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            if(ch == 'a') count_A++;
+            else if(ch == 'b') count_B++;
+            else if(ch == 'c') count_C++;
 
-            while(map.size() == 3)
-            {
-                ct = ct + (len - j);
-                ch = s.charAt(i);
-                if(map.get(ch) == 1)
+            if(count_A > 0 && count_B > 0 && count_C > 0){
+                ct = ct + len - j;
+
+                while(i < j)
                 {
-                    map.remove(ch);
+                    ch = s.charAt(i);
+                    if(ch == 'a') count_A--;
+                    else if(ch == 'b') count_B--;
+                    else if(ch == 'c') count_C--;
+                    i++;
+                    if(count_A > 0 && count_B > 0 && count_C > 0) ct = ct + len - j;
+                    else break;
                 }
-                else
-                {
-                    map.put(ch, map.get(ch) - 1);
-                }
-                i++;
             }
+            
+            j++;            
+        }
 
-            j++;
-        } 
-        
         return ct;
     }
 }
