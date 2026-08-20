2454af38fe1f6b9d7f3dda460b264076146aeae3
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2026-08-20 22:14:09 +0530

    Time: 2 ms (57.45%) | Memory: 46.7 MB (66.35%) - LeetSync

---
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] resultArray(int[] nums) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        list1.add(nums[0]);
+        list2.add(nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            if (list1.get(list1.size() - 1) > list2.get(list2.size() - 1)) {
+                list1.add(nums[i]);
+            } else {
+                list2.add(nums[i]);
+            }
+        }
+
+        int j = 0;
+
+        for (int num : list1) {
+            nums[j++] = num;
+        }
+
+        for (int num : list2) {
+            nums[j++] = num;
+        }
+
+        return nums;
+    }
+}
