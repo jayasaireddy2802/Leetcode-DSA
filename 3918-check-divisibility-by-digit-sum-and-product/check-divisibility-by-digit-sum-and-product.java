afb9b950c8f9d8ea2b32c31cd50eec656aaef977
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2026-08-22 11:58:38 +0530

    Time: 1 ms (25.54%) | Memory: 42.4 MB (62.34%) - LeetSync

--- 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.java
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.java
@@ -1,30 +1,47 @@
+// class Solution {
+
+//     public int sumOfDigits(int num)
+//     {
+//         int sum = 0;
+//         while(num > 0)
+//         {
+//             sum += num % 10;
+//             num = num / 10;
+//         }
+//         return sum;
+//     }
+
+//     public int productOfDigits(int num)
+//     {
+//         int val = 1;
+//         while(num > 0)
+//         {
+//             val *= num % 10;
+//             num = num / 10;
+//         }
+//         return val;
+//     }
+
+//     public boolean checkDivisibility(int n) {
+//         int sum = sumOfDigits(n);
+//         int product = productOfDigits(n);
+//         return (n % (sum + product) == 0) ? true : false;
+//     }
+// }
+
 class Solution {
-
-    public int sumOfDigits(int num)
-    {
-        int sum = 0;
-        while(num > 0)
-        {
-            sum += num % 10;
-            num = num / 10;
-        }
-        return sum;
-    }
-
-    public int productOfDigits(int num)
-    {
-        int val = 1;
-        while(num > 0)
-        {
-            val *= num % 10;
-            num = num / 10;
-        }
-        return val;
-    }
-
     public boolean checkDivisibility(int n) {
-        int sum = sumOfDigits(n);
-        int product = productOfDigits(n);
+        int product = 1;
+        int sum = 0;
+        int num = n;
+        while(num > 0)
+        {
+            int rem = num % 10;
+            sum += rem;
+            product *= rem;
+            num = num / 10;
+        }
         return (n % (sum + product) == 0) ? true : false;
     }
 }
+
