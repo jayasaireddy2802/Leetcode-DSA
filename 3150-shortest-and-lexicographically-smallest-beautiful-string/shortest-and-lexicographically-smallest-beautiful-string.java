122092cb8bd5a722f212b7d6cf671b8943d67d90
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2026-08-26 14:43:30 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (40.94%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,7 +1,56 @@
+// class Solution {
+//     public String shortestBeautifulSubstring(String s, int k) {
+//         int len = s.length();
+//         List<String> list = new ArrayList<>();
+//         int res = len;
+
+//         int i = 0;
+//         int j = 0;
+//         int ones = 0;
+
+//         while(j < len)
+//         {
+//             if(s.charAt(j) == '1')
+//                 ones++;
+
+//             while(ones > k)
+//             {
+//                 if(s.charAt(i) == '1')
+//                     ones--;
+//                 i++;
+//             }
+
+//             while (ones == k && s.charAt(i) == '0') {
+//                 i++;
+//             }
+
+//             int size = j - i + 1;
+//             if(size < res && ones == k)
+//             {
+//                 res = size;
+//                 list.clear();
+//                 list.add(s.substring(i, j + 1));
+//             }
+//             else if(size == res && ones == k)
+//             {
+//                 list.add(s.substring(i, j + 1));
+//             }
+
+//             j++;
+//         }
+
+//         Collections.sort(list);
+
+//         return list.size() == 0 ? "" : list.get(0);
+//     }
+// }
+
+
+
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
         int len = s.length();
-        List<String> list = new ArrayList<>();
+        String str = "";
         int res = len;
 
         int i = 0;
@@ -25,22 +74,15 @@ class Solution {
             }
 
             int size = j - i + 1;
-            if(size < res && ones == k)
+            if(ones == k && (str.length() == 0 || size < res || (size == res && s.substring(i, j + 1).compareTo(str) < 0)))
             {
                 res = size;
-                list.clear();
-                list.add(s.substring(i, j + 1));
+                str = s.substring(i, j + 1);
             }
-            else if(size == res && ones == k)
-            {
-                list.add(s.substring(i, j + 1));
-            }
-
+    
             j++;
         }
 
-        Collections.sort(list);
-
-        return list.size() == 0 ? "" : list.get(0);
+        return str;
     }
 }
