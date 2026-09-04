42540ea8e1e905a5239d2bab7a7706c055b8bfdd
jayasaireddy2802 <109647123+jayasaireddy2802@users.noreply.github.com>
2026-09-04 14:37:54 +0530

    Time: 1 ms (99.79%) | Memory: 46 MB (94.47%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.java
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -1,16 +1,40 @@
+// class Solution {
+//     public int firstStableIndex(int[] nums, int k) {
+//         int len = nums.length;
+//         int[] max = new int[len];
+//         int[] min = new int[len];
+//         int maxi = Integer.MIN_VALUE;
+//         int mini = Integer.MAX_VALUE;
+
+//         for(int i = 0; i < len; i++)
+//         {
+//             maxi = Math.max(maxi, nums[i]);
+//             max[i] = maxi;
+//         }
+
+//         for(int i = len - 1; i >= 0; i--)
+//         {
+//             mini = Math.min(mini, nums[i]);
+//             min[i] = mini;
+//         }
+
+//         for(int i = 0; i < len; i++)
+//         {
+//             if(max[i] - min[i] <= k) 
+//                 return i;
+//         }
+
+//         return -1;
+//     }
+// }
+
+
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int len = nums.length;
-        int[] max = new int[len];
         int[] min = new int[len];
         int maxi = Integer.MIN_VALUE;
         int mini = Integer.MAX_VALUE;
-
-        for(int i = 0; i < len; i++)
-        {
-            maxi = Math.max(maxi, nums[i]);
-            max[i] = maxi;
-        }
 
         for(int i = len - 1; i >= 0; i--)
         {
@@ -20,7 +44,8 @@ class Solution {
 
         for(int i = 0; i < len; i++)
         {
-            if(max[i] - min[i] <= k) 
+            maxi = Math.max(maxi, nums[i]);
+            if(maxi - min[i] <= k) 
                 return i;
         }
 
